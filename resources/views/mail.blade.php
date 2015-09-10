68f742bd44cd0d48365b694b78caa27ca7597d97
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2015-09-09 21:23:15 -0300

    Fix mail

--- resources/views/mail.blade.php
+++ resources/views/mail.blade.php
@@ -3,7 +3,7 @@
 <table width="100%" border="0" cellspacing="0" cellpadding="0">
     <tr>
         <td align="center">
-            <img src="http://www.parlamento-juvenil.rj.gov.br/files/convitesweb/{{ $person[10] }}"
+            <img src="http://www.parlamento-juvenil.rj.gov.br/files/convitesweb/{!! $person[10] !!} "
                  alt="Convite - Modernização do Legislativo Fluminense">
         </td>
     </tr>
