dfd432cd844807b71508e260c8b8a26c92a3ad16
vagrant <vagrant@homestead>
2016-02-25 20:21:29 +0000

    Update Clean CSS

--- resources/views/committees/meioambiente.blade.php
+++ resources/views/committees/meioambiente.blade.php
@@ -27,7 +27,7 @@
 
 @section('content-sidebar')
     @include('partials.committee-telephone', [
-         'title' => 'DEFESA DO CONSUMIDOR',
+         'title' => 'DISQUE MEIO AMBIENTE',
          'telephone' => '0800 023 0007',
          'site' => '<a href="http://www.alerj.rj.gov.br/cdc/" target="_blank"> <div class="link-site"><strong>http://www.alerj.rj.gov.br/cdc/</strong></div> </a>',
      ])
--- resources/views/partials/committee-telephone.blade.php
+++ resources/views/partials/committee-telephone.blade.php
@@ -1,12 +1,11 @@
-
-    <div class="sidebar-content">
-        <div class="infos text-center">
-            <p class="titulo-comissao">{{ $title }}</p>
-            <div class="linha-info"></div>
-            <div class="tel-comissao">{{ $telephone }}</div>
-            <div class="linha-info"></div>
-            {!! isset($site) ? $site : '' !!}
-            <p class="outras-info text-center">Atendimento das 8h às 20h</p>
-        </div>
+<div class="sidebar-content">
+    <div class="infos text-center">
+        <p class="titulo-comissao">{{ $title }}</p>
+        <div class="linha-info"></div>
+        <div class="tel-comissao">{{ $telephone }}</div>
+        <div class="linha-info"></div>
+        {!! isset($site) ? $site : '' !!}
+        <p class="outras-info text-center">Atendimento das 8h às 20h</p>
     </div>
+</div>
 
