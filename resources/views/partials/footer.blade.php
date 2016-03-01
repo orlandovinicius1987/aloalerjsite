8bb04a31489c9a02ab639a8a6e0ecd9eac029de1
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-01 18:18:35 -0300

    Add new footer

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -60,6 +60,7 @@
                             <p><a href="/comissoes/pessoadeficiente">Disque Pessoa Portadora de Deficiência</a></p>
                             <p><a href="/comissoes/saneamentoambiental">Disque Saneamento Ambiental</a></p>
                             <p><a href="/comissoes/segurancapublica">Disque Segurança Pública</a></p>
+                            <p><a href="https://aloalerjsite.antoniocarlosribeiro.com/tests/busca.html">Prova de Conceito Alô Alerj</a></p>
                         </div>
                         <div class="col-md-1"></div>
                         <div class="col-md-3">
