83ec251235b046497b492a358ad26102c921208b
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-03-01 18:06:27 +0000

    new slider

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -50,23 +50,61 @@
     })
 </script>
 
-<script type="text/javascript">
-    jQuery(document).ready(function()
-    {
-        jQuery('.slider').slick({
-            dots: true,
-            infinite: true,
-            speed: 500,
-            //fade: true,
-            cssEase: 'linear',
-            slidesToScroll: 1,
-            autoplay: true,
-            autoplaySpeed: 2000,
-            arrows: true,
-            draggable:true,
-            mobileFirst:true,
-            pauseOnDotsHover:true,
-            swipe: true
-        });
-    });
+{{--<script type="text/javascript">--}}
+    {{--jQuery(document).ready(function()--}}
+    {{--{--}}
+        {{--jQuery('.slider').slick({--}}
+            {{--dots: true,--}}
+            {{--infinite: true,--}}
+            {{--speed: 500,--}}
+            {{--//fade: true,--}}
+            {{--cssEase: 'linear',--}}
+            {{--slidesToScroll: 1,--}}
+            {{--autoplay: true,--}}
+            {{--autoplaySpeed: 2000,--}}
+            {{--arrows: true,--}}
+            {{--draggable:true,--}}
+            {{--mobileFirst:true,--}}
+            {{--pauseOnDotsHover:true,--}}
+            {{--swipe: true--}}
+        {{--});--}}
+    {{--});--}}
+{{--</script>--}}
+
+<script language="javascript">
+$(document).ready(function() {
+
+$("#owl-example").owlCarousel({
+
+    // Most important owl features
+    items : 1,
+    itemsCustom : false,
+    itemsDesktop : [1199,1],
+    itemsDesktopSmall : [980,1],
+    itemsTablet: [768,1],
+    itemsTabletSmall: false,
+    itemsMobile : [479,1],
+    singleItem : true,
+    itemsScaleUp : true,
+    //Basic Speeds
+    slideSpeed : 200,
+    paginationSpeed : 800,
+    rewindSpeed : 1000,
+    //Autoplay
+    autoPlay : true,
+    stopOnHover : false,
+    // Navigation
+    navigation : false,
+    rewindNav : true,
+    scrollPerPage : false,
+    // Responsive
+    responsive: true,
+    responsiveRefreshRate : 200,
+    responsiveBaseWidth: window,
+    //Auto height
+    autoHeight : true
+    }
+);
+
+});
 </script>
