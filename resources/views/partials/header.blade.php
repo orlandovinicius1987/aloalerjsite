dc4b948063e60fae7124a27bee7beeb8e2450974
Breno Trengrouse <brenot@gmail.com>
2016-03-07 19:32:49 +0000

    Agora foi - Ajustes Mobile Marcao

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -10,12 +10,29 @@
         @if (isset($home))
             <div class="row row-eq-height">
                 <div class="col-xs-12 col-sm-9 col-md-9 col-lg-9">
-                    <div class="col-xs-offset-3 col-xs-8 col-lg-3">
-
+                    <div class="hidden-xs col-sm-2 col-md-2 col-lg-2">
+                        <a href="/">
+                            <object type="image/svg+xml" data="/templates/mv/svg/logo-alerj-monocromatica.svg" class="alerj-logo">
+                                Alerj Logo <!-- fallback image in CSS -->
+                            </object>
+                        </a>
+                    </div>
+                    <div class="col-xs-offset-3 col-xs-8 visible-xs">
                         <object type="image/svg+xml" data="/templates/mv/svg/logo-alo-alerj-branca.svg" class="alolerj-logo-home img-responsive">
                             AloAlerj Logo
-                        </object>  </div>
+                        </object>
+                    </div>
+
+                    <div class="hidden-xs col-sm-3">
+                        <object type="image/svg+xml" data="/templates/mv/svg/logo-alo-alerj.svg" class="alolerj-logo img-responsive">
+                            AloAlerj Logo
+                        </object>
+                    </div>
+
+
+                    <div class="hidden-xs col-lg-6">@include('partials.telefones-uteis')</div>
                 </div>
+                <div class="sidebar-right-top hidden-xs col-sm-3 col-md-3 col-lg-3 text-center">@yield('sidebar-name')</div>
             </div>
         @else
             <div class="row row-eq-height">
@@ -28,7 +45,7 @@
                         </a>
                     </div>
                     <div class="col-xs-8 col-lg-3">@yield('page-name')</div>
-                    <div class="hidden-xs col-lg-7">@include('partials.telefones-uteis')</div>
+                    <div class="hidden-xs col-lg-6">@include('partials.telefones-uteis')</div>
                 </div>
                 <div class="sidebar-right-top hidden-xs col-sm-3 col-md-3 col-lg-3 text-center">@yield('sidebar-name')</div>
             </div>
