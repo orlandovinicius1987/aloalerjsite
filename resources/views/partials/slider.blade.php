755a5fb639d54970509db45c3e7398bad5e77320
Breno Trengrouse <brenot@gmail.com>
2016-03-08 21:17:25 +0000

    Testes Mobile - De grao em grao a galinha enche o papo

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,18 +1,18 @@
-<div id="slider" class="visible-xs visible-sm">
-    <a href="#" class="control_next">>></a>
-    <a href="#" class="control_prev"><<</a>
-    <ul>
-        <li><img src="/templates/mv/img/slides/slide-01.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-02.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-03.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-04.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-05.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-06.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-07.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-08.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-09.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-10.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-11.png"></li>
-        <li><img src="/templates/mv/img/slides/slide-12.png"></li>
-    </ul>
-</div>
+{{--<div id="slider" class="visible-xs visible-sm">--}}
+    {{--<a href="#" class="control_next">>></a>--}}
+    {{--<a href="#" class="control_prev"><<</a>--}}
+    {{--<ul>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-01.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-02.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-03.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-04.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-05.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-06.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-07.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-08.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-09.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-10.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-11.png"></li>--}}
+        {{--<li><img src="/templates/mv/img/slides/slide-12.png"></li>--}}
+    {{--</ul>--}}
+{{--</div>--}}
