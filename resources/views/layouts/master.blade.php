fcc826beb81a034db0eb66befdd0b98d2eaa14d6
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-08 13:59:08 -0300

    Show iframe size

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -35,6 +35,29 @@
 
         @include('partials.footer')
         @include('partials.scripts')
+
+        <script>
+            function alertSize() {
+                var myWidth = 0, myHeight = 0;
+                if( typeof( window.innerWidth ) == 'number' ) {
+                    //Non-IE
+                    myWidth = window.innerWidth;
+                    myHeight = window.innerHeight;
+                } else if( document.documentElement && ( document.documentElement.clientWidth || document.documentElement.clientHeight ) ) {
+                    //IE 6+ in 'standards compliant mode'
+                    myWidth = document.documentElement.clientWidth;
+                    myHeight = document.documentElement.clientHeight;
+                } else if( document.body && ( document.body.clientWidth || document.body.clientHeight ) ) {
+                    //IE 4 compatible
+                    myWidth = document.body.clientWidth;
+                    myHeight = document.body.clientHeight;
+                }
+                window.alert( 'Width = ' + myWidth + ' -- ' +'Height = ' + myHeight);
+            }
+
+            alertSize();
+        </script>
+
     </body>
 </html>
 
