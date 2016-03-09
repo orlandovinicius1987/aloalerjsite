bf45b30731683938cc369338804855235918b0aa
Breno Trengrouse <brenot@gmail.com>
2016-03-09 14:05:11 +0000

    Testes Mobile - De grao em grao a galinha enche o papo 7 - Carroussel Sliders

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -1,41 +1,126 @@
 <div id="myCarousel" class="carousel slide" data-ride="carousel">
     <!-- Indicators -->
     <ol class="carousel-indicators">
-        <li data-target="#myCarousel" data-slide-to="0" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="0" class="active"></li>
         <li data-target="#myCarousel" data-slide-to="1" class=""></li>
-        <li data-target="#myCarousel" data-slide-to="2" class="active"></li>
+        <li data-target="#myCarousel" data-slide-to="2" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="3" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="4" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="5" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="6" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="7" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="8" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="9" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="10" class=""></li>
+        <li data-target="#myCarousel" data-slide-to="11" class=""></li>
     </ol>
     <div class="carousel-inner" role="listbox">
-        <div class="item">
-            <img class="first-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="First slide">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>Example headline.</h1>
-                    <p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>
-                    <p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Sign up today</a></p>
-                </div>
-            </div>
-        </div>
-        <div class="item">
-            <img class="second-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Second slide">
-            <div class="container">
-                <div class="carousel-caption">
-                    <h1>Another example headline.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Learn more</a></p>
-                </div>
-            </div>
-        </div>
         <div class="item active">
-            <img class="third-slide" src="data:image/gif;base64,R0lGODlhAQABAIAAAHd3dwAAACH5BAAAAAAALAAAAAABAAEAAAICRAEAOw==" alt="Third slide">
+            <img class="first-slide" src="/templates/mv/img/slides/slide-01.png" alt="First slide">
             <div class="container">
                 <div class="carousel-caption">
-                    <h1>One more for good measure.</h1>
-                    <p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
-                    <p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>
+                    {{--<h1>Example headline.</h1>--}}
+                    {{--<p>Note: If you're viewing this page via a <code>file://</code> URL, the "next" and "previous" Glyphicon buttons on the left and right might not load/display properly due to web browser security rules.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Sign up today</a></p>--}}
                 </div>
             </div>
         </div>
+        <div class="item">
+            <img class="second-slide" src="/templates/mv/img/slides/slide-02.png" alt="Second slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>Another example headline.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Learn more</a></p>--}}
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="third-slide" src="/templates/mv/img/slides/slide-03.png" alt="Third slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="fourth-slide" src="/templates/mv/img/slides/slide-04.png" alt="fourth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="sixth-slide" src="/templates/mv/img/slides/slide-05.png" alt="sixth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="seventh-slide" src="/templates/mv/img/slides/slide-06.png" alt="seventh slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="eight-slide" src="/templates/mv/img/slides/slide-07.png" alt="eight slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+        <div class="item">
+            <img class="ninth-slide" src="/templates/mv/img/slides/slide-08.png" alt="ninth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+
+
+        <div class="item">
+            <img class="tenth-slide" src="/templates/mv/img/slides/slide-09.png" alt="tenth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+
+
+        <div class="item">
+            <img class="eleventh-slide" src="/templates/mv/img/slides/slide-11.png" alt="eleventh slide">
+            <div class="container">
+                <div class="carousel-caption">
+                    {{--<h1>One more for good measure.</h1>--}}
+                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
+                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
+                </div>
+            </div>
+        </div>
+
+
     </div>
     <a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev">
         <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
