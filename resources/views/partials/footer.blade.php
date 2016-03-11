d4d79202b29c968e3cc8cfa323db35dc4c38f41b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-10 21:00:45 -0300

    chat POC 2

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -12,7 +12,8 @@
             <p><a href="/comissoes/pessoadeficiente">Disque Pessoa Portadora de Deficiência</a></p>
             <p><a href="/comissoes/saneamentoambiental">Disque Saneamento Ambiental</a></p>
             <p><a href="/comissoes/segurancapublica">Disque Segurança Pública</a></p>
-            <p><a href="/templates/mv/Chat/chat_novo_responsivo2.html">Prova de Conceito Alô Alerj</a></p>
+            <p><a href="/templates/mv/Chat/chat_novo_responsivo2.html">POC Breno</a></p>
+            <p><a href="/templates/mv/Chat/chat.html">POC Marcão</a></p>
         </div>
         <div class="col-xs-2"></div>
         <div class="col-xs-8 col-sm-4 footer-logo-alerj">
@@ -62,7 +63,8 @@
                             <p><a href="/comissoes/pessoadeficiente">Disque Pessoa Portadora de Deficiência</a></p>
                             <p><a href="/comissoes/saneamentoambiental">Disque Saneamento Ambiental</a></p>
                             <p><a href="/comissoes/segurancapublica">Disque Segurança Pública</a></p>
-                            <p><a href="/templates/mv/Chat/chat_novo_responsivo2.html">Prova de Conceito Alô Alerj</a></p>
+                            <p><a href="/templates/mv/Chat/chat_novo_responsivo2.html">POC Breno</a></p>
+                            <p><a href="/templates/mv/Chat/chat.html">POC Marcão</a></p>
                         </div>
                         <div class="col-md-1"></div>
                         <div class="col-md-3">
