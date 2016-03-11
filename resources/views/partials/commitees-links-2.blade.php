cff1b165286610334cf1ba7cf363a9da9126c660
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-11 17:01:22 -0300

    Add busca

--- resources/views/partials/commitees-links-2.blade.php
+++ resources/views/partials/commitees-links-2.blade.php
@@ -3,6 +3,7 @@
 <p><a href="/comissoes/segurancapublica">Disque Segurança Pública</a></p>
 <p><a href="/templates/mv/chat/chat_novo_responsivo2.html">POC Breno</a></p>
 <p><a href="/templates/mv/chat/chat.html">POC Marcão</a></p>
+<p><a href="/tests/busca.html">POC ACR</a></p>
 <p><a href="http://stevesanderson.github.io/fixed-height-layouts-demo/simple.html">Teste Fixed Header</a></p>
 <p><a href="/tv">TV Alerj Ao Vivo</a></p>
 {{--<p><a href="/templates/mv/radio/index.html"></a></p>--}}
