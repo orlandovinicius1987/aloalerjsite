2a6b229e7a2d20b27e48c2766aeadf3193bdaae2
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-13 13:13:24 -0300

    Open on browser

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,9 +5,9 @@ Route::get('/', ['as' => 'home', 'uses' => 'Home@index']);
 Route::get('comissoes/{name}', ['as' => 'page', 'uses' => 'Committees@view']);
 
 Route::group(['prefix' => 'chat'], function() {
-    Route::get('create', ['as' => 'home', 'uses' => 'Chat@create']);
-    Route::get('index', ['as' => 'home', 'uses' => 'Chat@index']);
-    Route::get('terminated', ['as' => 'home', 'uses' => 'Chat@terminated']);
+    Route::get('index', ['as' => 'chat.index', 'uses' => 'Chat@index']);
+    Route::get('create', ['as' => 'chat.create', 'uses' => 'Chat@create']);
+    Route::get('terminated', ['as' => 'chat.terminated', 'uses' => 'Chat@terminated']);
 });
 
 Route::group(['prefix' => 'tv'], function() {
--- resources/views/partials/commitees-links-2.blade.php
+++ resources/views/partials/commitees-links-2.blade.php
@@ -1,7 +1,7 @@
 <p><a href="/comissoes/pessoadeficiente">Disque Pessoa Portadora de Deficiência</a></p>
 <p><a href="/comissoes/saneamentoambiental">Disque Saneamento Ambiental</a></p>
 <p><a href="/comissoes/segurancapublica">Disque Segurança Pública</a></p>
-<p><a href="/templates/mv/chat/chat_novo_responsivo2.html">POC Breno</a></p>
+<a href="#" onclick="window.open('{{ route('home') }}/templates/mv/chat/chat_novo_responsivo2.html', '_system');">POC Breno</a>
 <p><a href="/templates/mv/chat/chat.html">POC Marcão</a></p>
 <p><a href="/tests/busca.html">POC ACR</a></p>
 <p><a href="/templates/mv/fixed_header/index.html">POC Fabiano</a></p>
@@ -10,3 +10,4 @@
 {{--<p><a href="/templates/mv/radio/index.html"></a></p>--}}
 <p><a href='http://www.agenciaradioweb.com.br/novosite4/alerj/jw.php' title='Play'>Radio Alerj</a></p>
 <p><a href="#" id="scrollToTop" title='Play'>Scroll</a></p>
+
