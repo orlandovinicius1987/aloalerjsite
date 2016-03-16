a0dafbdb4d36f1116cd354bffd13574b6a7d8fc3
Fabiano Albernaz <fabiano.albernaz@gmail.com>
2016-03-16 17:02:55 +0000

    Radio Preload None

--- resources/views/radio/index.blade.php
+++ resources/views/radio/index.blade.php
@@ -14,7 +14,7 @@
     <div class="radio-content text-center">
         {{--<img src="/templates/mv/img/radio-alerj.png" alt="" class="">--}}
 
-        <audio controls preload="preload">
+        <audio controls>
             <source src="//centova10.ciclanohost.com.br:6044/live.mp3" type="audio/mpeg">
             Seu browser não suporta o elemento de audio.
         </audio>
