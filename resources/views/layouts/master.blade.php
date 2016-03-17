16acbe8f1e39bf504b0138a4d6d567c311a69359
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-17 05:36:14 -0300

    Randmize

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -10,10 +10,10 @@
         <link href="//fonts.googleapis.com/css?family=Montserrat" rel="stylesheet">
         <!-- Custom styles for this template -->
         <link href="/templates/mv/css/carousel.css" rel="stylesheet">
-        <link rel="stylesheet" type="text/css" href="/templates/mv/css/custom.css">
+        <link rel="stylesheet" type="text/css" href="/templates/mv/css/custom.css?breno={{rand(0,5000)}}">
 
         @if (Input::get('client') == 'app')
-            <link rel="stylesheet" href="/templates/mv/app.css">
+            <link rel="stylesheet" href="/templates/mv/app.css?breno={{rand(0,5000)}}">
         @endif
     </head>
 
