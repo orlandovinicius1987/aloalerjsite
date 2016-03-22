f6b67e61d82925259e5c3d64c2c07dd0d83145f6
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-22 12:18:23 -0300

    Change text

--- resources/views/contact/mailsent.blade.php
+++ resources/views/contact/mailsent.blade.php
@@ -4,6 +4,6 @@
     <div class="spacer"></div>
 
     <h1>
-        Prezado(a) Sr(a). {{ $name }}, sua mensagem foi recebida, obrigado!
+        {{ $name }}, sua mensagem foi recebida, obrigado!
     </h1>
 @stop
