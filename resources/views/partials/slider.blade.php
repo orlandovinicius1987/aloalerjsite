9c2bee26992cfbe92a27bbc4662aefcff5f58698
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-23 12:41:15 -0300

    remove slider arrows

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -131,37 +131,14 @@
                 </div>
             </div>
         </div>
-
-
-
-
     </div>
-    <a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev">
-        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-        <span class="sr-only">Previous</span>
-    </a>
-    <a class="right carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="next">
-        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-        <span class="sr-only">Next</span>
-    </a>
+
+    {{--<a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev">--}}
+        {{--<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>--}}
+        {{--<span class="sr-only">Previous</span>--}}
+    {{--</a>--}}
+    {{--<a class="right carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="next">--}}
+        {{--<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>--}}
+        {{--<span class="sr-only">Next</span>--}}
+    {{--</a>--}}
 </div><!-- /.carousel -->
-
-
-{{--<div id="slider" class="visible-xs visible-sm">--}}
-    {{--<a href="#" class="control_next">>></a>--}}
-    {{--<a href="#" class="control_prev"><<</a>--}}
-    {{--<ul>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-01.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-02.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-03.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-04.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-05.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-06.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-07.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-08.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-09.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-10.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-11.png"></li>--}}
-        {{--<li><img src="/templates/mv/img/slides/slide-12.png"></li>--}}
-    {{--</ul>--}}
-{{--</div>--}}
