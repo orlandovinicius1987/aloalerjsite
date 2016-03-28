a5d068c45b2233669c4d0f5ae3a02e614c18fad7
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-03-28 05:11:22 -0300

    Mail pretend

---
+++ resources/views/emails/contact.blade.php
@@ -0,0 +1,11 @@
+<html>
+    <body>
+        <table width="100%" border="0" cellspacing="0" cellpadding="0">
+            <tr>
+                <td align="center">
+                    Mensagem a ser enviada!
+                </td>
+            </tr>
+        </table>
+    </body>
+</html>
