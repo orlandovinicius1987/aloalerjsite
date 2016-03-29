223180dc4b1e8a3edaa9c5984fbf475c9958de32
Breno Trengrouse <brenot@gmail.com>
2016-03-29 15:11:27 +0000

    FOOTER - limpeza

--- resources/views/partials/commitees-links-2.blade.php
+++ resources/views/partials/commitees-links-2.blade.php
@@ -1,13 +1,6 @@
-<p><a href="/comissoes/pessoadeficiente">Disque Pessoa Portadora de Deficiência</a></p>
 <p><a href="/comissoes/saneamentoambiental">Disque Saneamento Ambiental</a></p>
 <p><a href="/comissoes/segurancapublica">Disque Segurança Pública</a></p>
-<p><a href="{{ route('home') }}/templates/mv/chat/chat_novo_responsivo2.html">POC Breno</a></p>
-<p><a href="/templates/mv/chat/chat.html">POC Marcão</a></p>
-<p><a href="/tests/busca.html">POC ACR</a></p>
-<p><a href="/templates/mv/fixed_header/index.html">POC Fabiano</a></p>
-<p><a href="http://stevesanderson.github.io/fixed-height-layouts-demo/simple.html">Teste Fixed Header</a></p>
 <p><a href="/tv">TV Alerj Ao Vivo</a></p>
 <p><a href='/radio' title='Play'>Radio Alerj</a></p>
-<p><a href="javascript: window.open('http://kallzenter.com/', '_system'); return false;">Site Externo</a></p>
 <p><a href='/contact' title='Play'>Fale Conosco</a></p>
 <p><a href='http://www.carteiradadobem.com.br/' title='Play'>Carteirada do Bem</a></p>
