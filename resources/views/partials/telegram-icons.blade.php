d265092f04233bd5bafc028601338f78c89702d3
Breno Trengrouse <brenot@gmail.com>
2016-04-01 20:33:59 +0000

    Centralizar Icones Whatsapp

--- resources/views/partials/telegram-icons.blade.php
+++ resources/views/partials/telegram-icons.blade.php
@@ -1,11 +1,11 @@
 
-<div class="col-xs-offset-1 col-xs-5 col-sm-5">
+<div class="col-xs-6 col-sm-6">
     <a href="https://www.whatsapp.com/?l=pt_br" target="_blank" class="whatsapp">
         <img src="/templates/mv/svg/whatsapp.svg" class="img-responsive" alt="whatsapp">
         <p class="leg-whatsapp">WhatsApp</p>
     </a>
 </div>
-<div class="col-xs-5 col-sm-5">
+<div class="col-xs-6 col-sm-6">
     <a href="https://telegram.org" target="_blank" class="telegram">
         <img src="/templates/mv/svg/telegram.svg" class="img-responsive" alt="telegram">
         <p class="leg-telegram">Telegram</p>
