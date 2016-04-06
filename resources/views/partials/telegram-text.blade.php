529100746995e1909b9a320f90da53714f5e8814
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-04-06 17:57:19 -0300

    Add whatsapp link

--- resources/views/partials/telegram-text.blade.php
+++ resources/views/partials/telegram-text.blade.php
@@ -7,6 +7,8 @@
 </div>
 <div class="row">
     <div class="col-xs-12">
-        <div class="telegra-telefone"><strong class="telefone">(21) 98890 4742</strong></div>
+        <div class="telegra-telefone"><strong class="telefone"><a href="intent://send/5521988904742#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end">(21) 98890 4742</a></strong></div>
     </div>
 </div>
+
+
