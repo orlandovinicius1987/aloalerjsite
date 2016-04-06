b7b459ba86077948500db6f7b8db9a09391c2d3f
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-04-06 20:29:01 -0300

    Fix link colors and decoration

--- resources/views/partials/committee-telephone.blade.php
+++ resources/views/partials/committee-telephone.blade.php
@@ -2,7 +2,7 @@
     <div class="infos text-center">
         <p class="titulo-comissao">{{ $title }}</p>
         <div class="linha-info"></div>
-        <div class="tel-comissao">{{ $telephone }}</div>
+        <div class="tel-comissao"><a href="tel:{{ $telephone }}" class="tel-comissao-link">{{ $telephone }}</a></div>
         <div class="linha-info"></div>
         {!! isset($site) ? $site : '' !!}
         <p class="outras-info text-center">
--- resources/views/partials/telegram-text.blade.php
+++ resources/views/partials/telegram-text.blade.php
@@ -7,8 +7,6 @@
 </div>
 <div class="row">
     <div class="col-xs-12">
-        <div class="telegra-telefone"><strong class="telefone"><a href="whatsapp://send/5521988904742#Intent;scheme=smsto;package=com.whatsapp;action=android.intent.action.SENDTO;end">(21) 98890 4742</a></strong></div>
+        <div class="telegra-telefone"><strong class="telefone"><a href="tel:988904742" class="telefone-link">(21) 98890 4742</a></strong></div>
     </div>
 </div>
-
-
