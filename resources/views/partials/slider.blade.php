287a7fd15e60f51afa945ffac4c3d08fb935988d
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2016-04-11 17:20:45 -0300

    Update slides

--- resources/views/partials/slider.blade.php
+++ resources/views/partials/slider.blade.php
@@ -147,17 +147,6 @@
                 </div>
             </div>
         </div>
-
-        <div class="item">
-            <img class="eleventh-slide" src="/templates/mv/img/slides/slide012.jpg" alt="eleventh slide">
-            <div class="container">
-                <div class="carousel-caption">
-                    {{--<h1>One more for good measure.</h1>--}}
-                    {{--<p>Cras justo odio, dapibus ac facilisis in, egestas eget quam. Donec id elit non mi porta gravida at eget metus. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>--}}
-                    {{--<p><a class="btn btn-lg btn-primary" href="http://getbootstrap.com/examples/carousel/#" role="button">Browse gallery</a></p>--}}
-                </div>
-            </div>
-        </div>
     </div>
 
     {{--<a class="left carousel-control" href="http://getbootstrap.com/examples/carousel/#myCarousel" role="button" data-slide="prev">--}}
