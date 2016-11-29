68fd8722994cb5957e8b248a5088a73c280c3a08
brenot <brenot@gmail.com>
2016-11-29 15:17:51 +0000

    Telefones Uteis - Dados + Js

--- resources/views/partials/scripts.blade.php
+++ resources/views/partials/scripts.blade.php
@@ -170,6 +170,56 @@
 </script>
 
 
+<script>
+    $(function () {
+        /* BOOTSNIPP FULLSCREEN FIX */
+        if (window.location == window.parent.location) {
+            $('#back-to-bootsnipp').removeClass('hide');
+        }
+
+
+        $('[data-toggle="tooltip"]').tooltip();
+
+        $('#fullscreen').on('click', function(event) {
+            event.preventDefault();
+            window.parent.location = "http://bootsnipp.com/iframe/4l0k2";
+        });
+        $('a[href="#cant-do-all-the-work-for-you"]').on('click', function(event) {
+            event.preventDefault();
+            $('#cant-do-all-the-work-for-you').modal('show');
+        })
+
+        $('[data-command="toggle-search"]').on('click', function(event) {
+            event.preventDefault();
+            $(this).toggleClass('hide-search');
+
+            if ($(this).hasClass('hide-search')) {
+                $('.c-search').closest('.row').slideUp(100);
+            }else{
+                $('.c-search').closest('.row').slideDown(100);
+            }
+        })
+
+        $('#contact-list').searchable({
+            searchField: '#contact-list-search',
+            selector: 'li',
+            childSelector: '.col-xs-12',
+            show: function( elem ) {
+                elem.slideDown(100);
+            },
+            hide: function( elem ) {
+                elem.slideUp( 100 );
+            }
+        })
+    });
+
+</script>
+
+<!-- JavaScrip Search Plugin -->
+<script src="//rawgithub.com/stidges/jquery-searchable/master/dist/jquery.searchable-1.0.0.min.js"></script>
+
+
+
 <!-- Plugin JavaScript -->
 <script src="//cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.3/jquery.easing.min.js"></script>
 <script src="/templates/mv/js/classie.js"></script>
