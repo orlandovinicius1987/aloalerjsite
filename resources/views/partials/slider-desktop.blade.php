59338c91329d51bf3bf769c34aebfc5c8e8b369d
Breno Trengrouse <brenot@gmail.com>
2017-12-18 16:33:59 +0000

    Add Slider-Desktop

---
+++ resources/views/partials/slider-desktop.blade.php
@@ -0,0 +1,102 @@
+<!-- .carousel-desktop -->
+<div class="carousel slide" data-ride="carousel">
+
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img class="first-slide" src="/templates/mv/img/slides-desktop/slide000.jpg" alt="First slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="second-slide" src="/templates/mv/img/slides-desktop/slide001.jpg" alt="Second slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="third-slide" src="/templates/mv/img/slides-desktop/slide002.jpg" alt="Third slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="fourth-slide" src="/templates/mv/img/slides-desktop/slide003.jpg" alt="fourth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="sixth-slide" src="/templates/mv/img/slides-desktop/slide004.jpg" alt="sixth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="seventh-slide" src="/templates/mv/img/slides-desktop/slide005.jpg" alt="seventh slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="eight-slide" src="/templates/mv/img/slides-desktop/slide006.jpg" alt="eight slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="ninth-slide" src="/templates/mv/img/slides-desktop/slide007.jpg" alt="ninth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="tenth-slide" src="/templates/mv/img/slides-desktop/slide008.jpg" alt="tenth slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide009.jpg" alt="eleventh slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide010.jpg" alt="eleventh slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+
+        <div class="item">
+            <img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide011.jpg" alt="eleventh slide">
+            <div class="container">
+                <div class="carousel-caption">
+                </div>
+            </div>
+        </div>
+    </div>
+
+</div><!-- /.carousel-desktop -->
