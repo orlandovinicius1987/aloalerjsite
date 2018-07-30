169de6f4b12642b33d3f9b8d35a4a6f017adfa11
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-07-30 14:24:19 -0300

    Change home text

--- resources/views/partials/slider-desktop.blade.php
+++ resources/views/partials/slider-desktop.blade.php
@@ -1,102 +1,112 @@
-<!-- .carousel-desktop -->
-<div class="carousel slide" data-ride="carousel">
+<h1>Alô Alerj</h1>
 
-    <div class="carousel-inner" role="listbox">
-        <div class="item active">
-            <img class="first-slide" src="/templates/mv/img/slides-desktop/slide000.jpg" alt="First slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+<p>O Alô Alerj é um canal de comunicação entre você e a Assembleia Legislativa do Estado do Rio de Janeiro (Alerj). Somos uma ouvidoria, de caráter conciliatório e informativo, a serviço do cidadão. O nosso objetivo é ajudar você a solucionar o seu problema. Aqui você pode registrar reclamações, solicitações e sugestões sobre órgãos públicos e/ou privados.</p>
 
-        <div class="item">
-            <img class="second-slide" src="/templates/mv/img/slides-desktop/slide001.jpg" alt="Second slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+<p>Temos operadores preparados para atender você, de segunda a sexta-feira, das 8h às 19h. Ao ligar para o Alô Alerj tenha em mãos os seus dados para agilizar o atendimento. O registro de sua demanda sempre gera um número de protocolo, que é fornecido por nossos atendentes. Não esqueça de anotá-lo! É por meio desse número que você poderá acompanhar o andamento do seu processo.</p>
 
-        <div class="item">
-            <img class="third-slide" src="/templates/mv/img/slides-desktop/slide002.jpg" alt="Third slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+<h3>Como Funciona</h3>
 
-        <div class="item">
-            <img class="fourth-slide" src="/templates/mv/img/slides-desktop/slide003.jpg" alt="fourth slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+<p>Após o cadastro da sua demanda, ela é encaminhada para o órgão competente, que tem, a partir daí, 60 dias para responder. Você receberá em sua casa, por meio de carta, uma cópia do registro da reclamação e também a resposta do órgão competente, se houver. Após o prazo de 60 dias, caso não haja resposta, você pode ligar para o Alô Alerj e demandar que seja enviada uma notificação para a reclamada. Nosso equipe está pronta para auxiliá-lo.</p>
 
-        <div class="item">
-            <img class="sixth-slide" src="/templates/mv/img/slides-desktop/slide004.jpg" alt="sixth slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+{{--<!-- .carousel-desktop -->--}}
+{{--<div class="carousel slide" data-ride="carousel">--}}
 
-        <div class="item">
-            <img class="seventh-slide" src="/templates/mv/img/slides-desktop/slide005.jpg" alt="seventh slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+    {{--<div class="carousel-inner" role="listbox">--}}
+        {{--<div class="item active">--}}
+            {{--<img class="first-slide" src="/templates/mv/img/slides-desktop/slide000.jpg" alt="First slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
-        <div class="item">
-            <img class="eight-slide" src="/templates/mv/img/slides-desktop/slide006.jpg" alt="eight slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+        {{--<div class="item">--}}
+            {{--<img class="second-slide" src="/templates/mv/img/slides-desktop/slide001.jpg" alt="Second slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
-        <div class="item">
-            <img class="ninth-slide" src="/templates/mv/img/slides-desktop/slide007.jpg" alt="ninth slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+        {{--<div class="item">--}}
+            {{--<img class="third-slide" src="/templates/mv/img/slides-desktop/slide002.jpg" alt="Third slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
-        <div class="item">
-            <img class="tenth-slide" src="/templates/mv/img/slides-desktop/slide008.jpg" alt="tenth slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+        {{--<div class="item">--}}
+            {{--<img class="fourth-slide" src="/templates/mv/img/slides-desktop/slide003.jpg" alt="fourth slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
-        <div class="item">
-            <img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide009.jpg" alt="eleventh slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+        {{--<div class="item">--}}
+            {{--<img class="sixth-slide" src="/templates/mv/img/slides-desktop/slide004.jpg" alt="sixth slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
-        <div class="item">
-            <img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide010.jpg" alt="eleventh slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
+        {{--<div class="item">--}}
+            {{--<img class="seventh-slide" src="/templates/mv/img/slides-desktop/slide005.jpg" alt="seventh slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
-        <div class="item">
-            <img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide011.jpg" alt="eleventh slide">
-            <div class="container">
-                <div class="carousel-caption">
-                </div>
-            </div>
-        </div>
-    </div>
+        {{--<div class="item">--}}
+            {{--<img class="eight-slide" src="/templates/mv/img/slides-desktop/slide006.jpg" alt="eight slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
 
-</div><!-- /.carousel-desktop -->
+        {{--<div class="item">--}}
+            {{--<img class="ninth-slide" src="/templates/mv/img/slides-desktop/slide007.jpg" alt="ninth slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+        {{--<div class="item">--}}
+            {{--<img class="tenth-slide" src="/templates/mv/img/slides-desktop/slide008.jpg" alt="tenth slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+        {{--<div class="item">--}}
+            {{--<img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide009.jpg" alt="eleventh slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+        {{--<div class="item">--}}
+            {{--<img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide010.jpg" alt="eleventh slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+
+        {{--<div class="item">--}}
+            {{--<img class="eleventh-slide" src="/templates/mv/img/slides-desktop/slide011.jpg" alt="eleventh slide">--}}
+            {{--<div class="container">--}}
+                {{--<div class="carousel-caption">--}}
+                {{--</div>--}}
+            {{--</div>--}}
+        {{--</div>--}}
+    {{--</div>--}}
+
+{{--</div><!-- /.carousel-desktop -->--}}
