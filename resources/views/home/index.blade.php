769ae2040faeca01c8fcc1b92bc663f3d4a3c355
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-08-01 12:42:44 -0300

    Add info

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -13,6 +13,13 @@
 @section('content-main')
     <div class="hidden-xs hidden-sm hidden-md">
 
+        <br><br>
+        <div class="jumbotron">
+            <h1>ATENÇÃO</h1>
+            <p>O serviço Alô Alerj estará passando por uma reformulação nos próximos dias, o que poderá ocasionar sobrecarga nas linhas telefônicas que integram o sistema 0800. Lembramos que as mensagens também podem ser encaminhadas pelo site da Alerj, por meio do link
+                <a href="https://www.aloalerj.rj.gov.br/contact">https://www.aloalerj.rj.gov.br/contact</a>.</p>
+        </div>
+
         @include('partials.slider-desktop')
 
         <div class="row row-eq-height blocos">
