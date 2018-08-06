399e3bcf94e6b6de618c8ecc7b25b9d4ecd0067b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-08-06 18:34:08 -0300

    Fix messages

--- routes/console.php
+++ routes/console.php
@@ -1,42 +1,7 @@
 <?php
 
-use App\Data\Models\PersonModel;
+use App\Services\ImportCercred;
 
 Artisan::command('z:import', function () {
-    ini_set('memory_limit', '2048M');
-
-    $counter = 0;
-
-    PersonModel::truncate();
-
-    DB::connection('cercred')
-        ->table('pessoa')
-        ->get()
-        ->each(function ($person) use (&$counter) {
-            PersonModel::create([
-                'id' => $person->pessoa_id,
-                'code' => $person->codigo,
-                'name' => $person->nome,
-                'cpf_cnpj' => $person->cpf,
-                'identification' => $person->rg,
-                'birthdate' => $person->nascimento,
-                'gender_id' => $person->sexo,
-                'civil_status_id' => $person->estado_civil_id,
-                'spouse_name' => $person->nome_conjuge,
-                'main_occupation_id' => $person->ocupacao_principal,
-                'scholarship_id' => $person->escolaridade_id,
-                'income' => (float) str_replace('$', '', $person->renda),
-                'person_type_id' => $person->tipo_pessoa,
-                'created_at' => $person->inclusao,
-                'updated_by' => $person->usuario_id_alteracao,
-            ]);
-
-            $counter++;
-
-            if ($counter % 100 === 0) {
-                $this->info(
-                    "{$counter} = {$person->pessoa_id} - {$person->nome}"
-                );
-            }
-        });
+    app(ImportCercred::class)->import($this);
 })->describe('Display an inspiring quote');
