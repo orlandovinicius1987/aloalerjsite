2c4e77049be71de17bf4a396d597340bb74e0de0
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-08-07 16:48:09 -0300

    Improve design

--- resources/views/callcenter/person_addresses/index.blade.php
+++ resources/views/callcenter/person_addresses/index.blade.php
@@ -1,27 +1,31 @@
 <div class="card">
-    <div class="card-header">{{ __('Endereços') }}</div>
+    <div class="card-header">
+        {{ __('Endereços') }}
+
+        <a id="buttonEndereços" href="{{ route('personsAddresses.create',['person_id' => $person->id]) }}"
+           class="btn btn-primary btn-sm pull-right">
+            <i class="fa fa-plus"></i>
+            Novo Endereço
+        </a>
+    </div>
 
     <div class="card-body">
         <table id="addressesTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
             <thead>
-            <tr>
-                <th>Endereço</th>
-            </tr>
+                <tr>
+                    <th>#</th>
+                    <th>Endereço</th>
+                </tr>
             </thead>
+
             @forelse ($addresses as $address)
-            <tr>
-                <td><a href="{{ route('personsAddresses.show',['id' => $address->id]) }}">{{ $address->id }}</a></td>
-            </tr>
+                <tr>
+                    <td><a href="{{ route('personsAddresses.show',['id' => $address->id]) }}">{{ $address->id }}</a></td>
+                    <td><a href="{{ route('personsAddresses.show',['id' => $address->id]) }}">{{ $address->street }}</a></td>
+                </tr>
             @empty
-            <p>Nenhum Endereço encontrado</p>
+                <p>Nenhum Endereço encontrado</p>
             @endforelse
         </table>
-
     </div>
-
-    <a id="buttonEndereços" href="{{ route('personsAddresses.create',['person_id' => $person->id]) }}"
-       class="btn btn-primary pull-right">
-        <i class="fa fa-plus"></i>
-        Novo Endereço
-    </a>
 </div>
