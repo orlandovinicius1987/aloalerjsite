82424959200e66b5b307807b1993404d246adb90
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-08-07 15:42:09 -0300

    Fix controllers

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -8,7 +8,7 @@ use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Foundation\Auth\RegistersUsers;
 
-class Register  extends  Controller
+class RegisterController  extends  Controller
 {
     /*
     |--------------------------------------------------------------------------
--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,13 @@
 <?php
 
+use App\Data\Models\Person;
 use App\Services\ImportCercred;
 
 Artisan::command('cercred:import', function () {
     app(ImportCercred::class)->import($this);
 })->describe('Display an inspiring quote');
+
+Artisan::command('cercred:test', function () {
+    dd(Person::find(3)->addresses->toArray());
+})->describe('Display an inspiring quote');
+
