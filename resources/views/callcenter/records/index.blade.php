4fe6a5ef6080ef58f66c981509049c1f93ecdd9c
Orlando Vinícius Valença Ferreira da Silva <orlando.vinicius1987@gmail.com>
2018-08-21 14:35:15 +0000

    Revision da issue: #191

--- resources/views/callcenter/records/index.blade.php
+++ resources/views/callcenter/records/index.blade.php
@@ -14,32 +14,5 @@
     </a>
         @endif
 </div>
-
-<div class="card">
-    <div class="card-body">
-        <table id="recordsTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
-            <thead>
-                <tr>
-                    <th>Protocolos</th>
-                    <th>Assuntos</th>
-                    <th>Comissão</th>
-                    <th>Tipo de Protocolo</th>
-                    <th>Área</th>
-                </tr>
-            </thead>
-
-            @forelse ($records as $record)
-                <tr>
-                    <td><a href="{{ route('records.show',['id' => $record->id]) }}">{{ $record->protocol }}</a></td>
-                    <td>{{ $record->subject }}</td>
-                    <td>{{ $record->committee->name or '' }}</td>
-                    <td>{{ $record->recordType->name or '' }}</td>
-                    <td>{{ $record->area->name or '' }}</td>
-                </tr>
-            @empty
-                <p>Nenhum Protocolo encontrado</p>
-            @endforelse
-        </table>
-    </div>
-</div>
+@include('callcenter.records.table')
 @endsection
