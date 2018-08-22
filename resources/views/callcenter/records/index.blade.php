f81b034cfbeea4319d9a2cf8142dcef1061a85a1
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-08-22 15:28:32 -0300

    Fix table display

--- resources/views/callcenter/records/index.blade.php
+++ resources/views/callcenter/records/index.blade.php
@@ -1,56 +1,29 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="card mt-4">
-    <div class="card-header">
-        {{ __('Protocolos') }}
-<div class="card-header">
-    {{ __('Protocolos') }}
-    @if(isset($person))
-        <a id="buttonAndamentos"
-           href="{{ route('records.create',['person_id'=>$person->id]) }}"
-           class="btn btn-primary btn-sm pull-right"
-        >
-            <i class="fa fa-plus"></i>
-            Novo Protocolo
-        </a>
-    </div>
-        @endif
-    <div class="card-body">
-        <table id="recordsTable" class="table table-striped table-bordered" cellspacing="0" width="100%">
-            <thead>
-                <tr>
-                    <th>Protocolos</th>
-                    <th>Assuntos</th>
-                    <th>Comissão</th>
-                    <th>Tipo de Protocolo</th>
-                    <th>Área</th>
-                </tr>
-            </thead>
+    <div class="card mt-4">
+        <div class="card-header">
+            <div class="row"><div class="col-4">
+                <h5>
+                    {{ __('Protocolos') }}
+                </h5>
+            </div>
 
-            @forelse ($records as $record)
-                <tr>
-                    <td><a href="{{ route('records.show',['id' => $record->id]) }}">{{ $record->protocol }}</a></td>
-                    <td>{{ $record->subject }}</td>
-                    <td>{{ $record->committee->name or '' }}</td>
-                    <td>{{ $record->recordType->name or '' }}</td>
-                    <td>{{ $record->area->name or '' }}</td>
-                </tr>
-            @empty
-                <p>Nenhum Protocolo encontrado</p>
-            @endforelse
-        </table>
+            <div class="col-8 text-right">
+                @if(isset($person))
+                    <a id="buttonAndamentos"
+                       href="{{ route('records.create',['person_id'=>$person->id]) }}"
+                       class="btn btn-primary btn-sm pull-right"
+                    >
+                        <i class="fa fa-plus"></i>
+                        Novo Protocolo
+                    </a>
+                @endif
+            </div></div>
+        </div>
+
+        <div class="card-body">
+            @include('callcenter.records.table')
+        </div>
     </div>
-</div>
-    @if(isset($person))
-    <a id="buttonAndamentos"
-       href="{{ route('records.create',['person_id'=>$person->id]) }}"
-       class="btn btn-primary btn-sm pull-right"
-    >
-        <i class="fa fa-plus"></i>
-        Novo Protocolo
-    </a>
-        @endif
-</div>
-@include('callcenter.records.table')
 @endsection
