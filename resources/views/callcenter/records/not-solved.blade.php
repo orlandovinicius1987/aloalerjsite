164b0403317cb36a34fd1865ae8bc05e705cc9c3
Orlando Vinícius Valença Ferreira da Silva <orlando.vinicius1987@gmail.com>
2018-08-23 18:55:43 +0000

    Isue: #192

---
+++ resources/views/callcenter/records/not-solved.blade.php
@@ -0,0 +1,14 @@
+<div class="card mt-4">
+    <div class="card-header">
+        {{ __('Protocolos') }}
+    @if(isset($person))
+        <a id="buttonAndamentos"
+           href="{{ route('records.create',['person_id'=>$person->id]) }}"
+           class="btn btn-primary btn-sm pull-right"
+        >
+            <i class="fa fa-plus"></i>
+            Novo Protocolo
+        </a>
+    </div>
+        @endif
+@include('callcenter.records.table')
