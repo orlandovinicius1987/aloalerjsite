72cf994838bb83c45f5b225b92341295f8f477b3
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-08-28 11:21:28 -0300

    Tweak forms a bit

--- resources/views/callcenter/people/form.blade.php
+++ resources/views/callcenter/people/form.blade.php
@@ -6,27 +6,27 @@
 
         <div class="card-body">
             @if (session('status'))
-            <div class="alert alert-success">
-                {{ session('status') }}
-            </div>
+                <div class="alert alert-success">
+                    {{ session('status') }}
+                </div>
             @endif
 
             @if(session()->has('message'))
-            <div class="alert alert-success">
-                {{ session()->get('message') }}
-            </div>
+                <div class="alert alert-success">
+                    {{ session()->get('message') }}
+                </div>
             @endif
 
             @if (isset($message))
-            <div class="alert alert-success">
-                {{ $message }}
-            </div>
+                <div class="alert alert-success">
+                    {{ $message }}
+                </div>
             @endif
 
             @if(session()->has('warning'))
-            <div class="alert alert-warning">
-                {{ session()->get('warning') }}
-            </div>
+                <div class="alert alert-warning">
+                    {{ session()->get('warning') }}
+                </div>
             @endif
 
             <form method="POST" action="{{ route('persons.store') }}" aria-label="{{ __('Persons') }}">
@@ -41,68 +41,98 @@
                 @endif
 
                 <div class="form-group row">
-                    <label for="cpf_cnpj" class="col-sm-4 col-form-label text-md-right">{{ __('CNPJ/CPF')}}</label>
+                    <label for="cpf_cnpj" class="col-sm-2 col-form-label text-md-right">{{ __('CNPJ/CPF')}}</label>
 
-                    <div class="col-md-6">
+                    <div class="col-sm-4">
                         <input id="cpf_cnpj" type="cpf_cnpj"
-                               class="form-control{{ $errors->has('cpf_cnpj') ? ' is-invalid' : '' }}"
-                               name="cpf_cnpj"
-                               value="{{is_null(old('cpf_cnpj')) ? $person->cpf_cnpj : old('cpf_cnpj')}}"
-                               required autofocus
-                               v-mask='["###.###.###-##", "##.###.###/####-##"]'
-                               {{$person->id ? 'disabled="disabled"' : '' }}
+                           class="form-control{{ $errors->has('cpf_cnpj') ? ' is-invalid' : '' }}"
+                           name="cpf_cnpj"
+                           value="{{is_null(old('cpf_cnpj')) ? $person->cpf_cnpj : old('cpf_cnpj')}}"
+                           required autofocus
+                           v-mask='["###.###.###-##", "##.###.###/####-##"]'
+                           {{$person->id ? 'disabled="disabled"' : '' }}
                         >
 
                         @if ($errors->has('cpf_cnpj'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('cpf_cnpj') }}</strong>
-                        </span>
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('cpf_cnpj') }}</strong>
+                            </span>
                         @endif
                     </div>
-                </div>
 
-                <div class="form-group row">
-                    <label for="name" class="col-sm-4 col-form-label text-md-right">{{ __('Nome Completo')}}</label>
+                    <label for="identification" class="col-sm-2 col-form-label text-md-right">
+                        {{ __('RG')}}
+                    </label>
 
-                    <div class="col-md-6">
-                        <input id="name"
-                               class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
-                               name="name" value="{{is_null(old('name')) ? $person->name : old('name')}}"
-                               required autofocus>
-
-                        @if ($errors->has('name'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('name') }}</strong>
-                        </span>
-                        @endif
-                    </div>
-                </div>
-
-                <div class="form-group row">
-                    <label for="identification" class="col-sm-4 col-form-label text-md-right">{{ __('RG')}}</label>
-
-                    <div class="col-md-6">
+                    <div class="col-sm-4">
                         <input id="identification"
-                               class="form-control{{ $errors->has('identification') ? ' is-invalid' : '' }}"
-                               name="identification"
-                               value="{{is_null(old('identification')) ? $person->identification : old('identification')}}"
-                               required autofocus>
+                           class="form-control{{ $errors->has('identification') ? ' is-invalid' : '' }}"
+                           name="identification"
+                           value="{{is_null(old('identification')) ? $person->identification : old('identification')}}"
+                           required autofocus
+                        >
 
                         @if ($errors->has('identification'))
-                        <span class="invalid-feedback" role="alert">
-                            <strong>{{ $errors->first('identification') }}</strong>
-                        </span>
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('identification') }}</strong>
+                            </span>
                         @endif
                     </div>
                 </div>
 
+                <div class="form-group row">
+                    <label for="name" class="col-sm-2 col-form-label text-md-right">{{ __('Nome Completo')}}</label>
+
+                    <div class="col-sm-10">
+                        <input id="name"
+                           class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}"
+                           name="name" value="{{is_null(old('name')) ? $person->name : old('name')}}"
+                           required autofocus
+                        >
+
+                        @if ($errors->has('name'))
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $errors->first('name') }}</strong>
+                            </span>
+                        @endif
+                    </div>
+                </div>
+
+                <div class="form-group row">
+                    <label for="identification" class="col-sm-3 col-form-label text-md-right">
+                        {{ __('Criado em')}}
+                    </label>
+
+                    <div class="col-md-3">
+                        <input id="identification"
+                           class="form-control"
+                           value="{{ $person->created_at ?? '' }}"
+                           disabled
+                        >
+                    </div>
+
+                    <label for="identification" class="col-sm-3 col-form-label text-md-right">
+                        {{ __('Alterado em')}}
+                    </label>
+
+                    <div class="col-md-3">
+                        <input id="identification"
+                           class="form-control"
+                           value="{{ $person->updated_at ?? '' }}"
+                           disabled
+                        >
+                    </div>
+                </div>
+
+
                 <div class="form-group row mb-0">
-                    <div class="col-md-8 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
+                    <div class="col-md-12 text-right">
+                        <br>
+                        <button type="submit" class="btn btn-danger">
                             @if (isset($workflow) && $workflow)
-                            {{ __('Próximo passo >>') }}
+                                {{ __('Próximo passo >>') }}
                             @else
-                            {{ __('Gravar') }}
+                                {{ __('Gravar') }}
                             @endif
                         </button>
                     </div>
--- resources/views/callcenter/records/form.blade.php
+++ resources/views/callcenter/records/form.blade.php
@@ -246,7 +246,7 @@
 
                 <div class="form-group row mb-0">
                     <div class="col-md-8 offset-md-4">
-                        <button type="submit" class="btn btn-primary">
+                        <button type="submit" class="btn btn-danger">
                             @if (isset($workflow) && $workflow)
                                 {{ __('Próximo passo >>') }}
                             @else
