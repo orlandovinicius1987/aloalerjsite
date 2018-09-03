c3f6c1c0a0b7d694d1b6768591518d51ce786908
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-09-03 11:41:35 -0300

    Remove directive

--- resources/assets/js/apps/contact-outside-workflow.js
+++ resources/assets/js/apps/contact-outside-workflow.js
@@ -1,12 +1,5 @@
 const appName = 'vue-contact-outside-workflow'
 
-Vue.directive('init', {
-    bind: function(el, binding, vnode) {
-        console.info(binding.arg);
-        vnode.context.form[binding.arg] = binding.value;
-    }
-})
-
 if (jQuery("#" + appName).length > 0) {
     const app = new Vue({
         el: '#'+appName,
