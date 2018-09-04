db83dc0e6be7f7118c570d0e844d111aef0334bc
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-09-04 13:06:35 -0300

    Refactor dates a bit

--- app/Data/Presenters/Base.php
+++ app/Data/Presenters/Base.php
@@ -2,37 +2,10 @@
 
 namespace App\Data\Presenters;
 
-use Jenssegers\Date\Date;
+use App\Support\Dates;
 use McCool\LaravelAutoPresenter\BasePresenter as McCoolBasePresenter;
 
 class Base extends McCoolBasePresenter
 {
-    public function created_at_formatted()
-    {
-        return $this->formatDate($this->wrappedObject->created_at);
-    }
-
-    private function formatDate($date)
-    {
-        if (!$date) {
-            return '';
-        }
-
-        Date::setLocale('pt_BR');
-
-        return Date::parse($date)->format($this->getDateFormat());
-    }
-
-    private function getDateFormat()
-    {
-        return 'j F Y - H\hi';
-    }
-
-    public function updated_at_formatted()
-    {
-        return $this->wrappedObject->created_at ==
-        $this->wrappedObject->updated_at
-            ? ''
-            : $this->formatDate($this->wrappedObject->updated_at);
-    }
+    use Dates;
 }
