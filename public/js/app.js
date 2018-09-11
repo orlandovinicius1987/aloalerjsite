1879fb32e841a72310ff2d49e1e8a2c84ae49e9c
Alexandre Ferreira <alexandreee@gmail.com>
2018-09-11 19:17:12 -0300

    Quando se pesquisa apenas por CPF , o campo 'name' vai com valor NULL
    #299

--- public/js/app.js
+++ public/js/app.js
@@ -53075,7 +53075,7 @@ if (jQuery("#" + appName).length > 0) {
 
             form: {
                 search: {
-                    name: null,
+                    name: '',
                     cpf_cnpj: null
                 }
             }
