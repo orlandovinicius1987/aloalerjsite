e685e1d9d5b952a21b83bca2eac9456fa78aff89
Breno Trengrouse <breno@Ogum.local>
2018-09-18 15:59:47 -0300

    Buttons Style - General - Add Depth

--- resources/views/callcenter/people/form.blade.php
+++ resources/views/callcenter/people/form.blade.php
@@ -109,7 +109,7 @@
                 <div class="form-group row mb-0">
                     <div class="col-md-12 text-right">
                         <br>
-                        <button id="saveButton" type="submit" class="btn btn-danger">
+                        <button id="saveButton" type="submit" class="btn btn-danger btn-depth">
                             @if ($workflow)
                                 Próximo passo >>
                             @else
--- resources/views/callcenter/person_contacts/form-workflow.blade.php
+++ resources/views/callcenter/person_contacts/form-workflow.blade.php
@@ -142,7 +142,7 @@
 
                 <div class="form-group row mb-0">
                     <div class="col-md-8 offset-md-4">
-                        <button id="saveButton" type="submit" class="btn btn-danger">
+                        <button id="saveButton" type="submit" class="btn btn-danger btn-depth">
                             Gravar
                         </button>
                     </div>
