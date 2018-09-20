d5760ec8291e158db6728f04477031a3170b3fc2
Breno Trengrouse <breno@Ogum.local>
2018-09-20 11:54:29 -0300

    Styling Committees View - Removind Cards

--- resources/views/callcenter/committees/index.blade.php
+++ resources/views/callcenter/committees/index.blade.php
@@ -1,25 +1,23 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="card mt-4">
-        <div class="card-header">
+    <div class="mt-4">
+        <div class="row align-items-center">
+            <div class="col-4">
+                <h2>
+                    Comissões
+                </h2>
+            </div>
 
-            <div class="row align-items-center">
-                <div class="col-4">
-                    <h5>
-                        Comissões
-                    </h5>
-                </div>
-
-                <div class="col-8 text-right">
-                    <a id="buttonEndereços" href="{{ route('committees.create') }}"
-                       class="btn btn-primary btn-sm pull-right btn-depth">
-                        <i class="fa fa-plus"></i>
-                        Nova Commissão
-                    </a>
-                </div>
+            <div class="col-8 text-right">
+                <a id="buttonEndereços" href="{{ route('committees.create') }}"
+                   class="btn btn-primary btn-sm pull-right btn-depth">
+                    <i class="fa fa-plus"></i>
+                    Nova Commissão
+                </a>
             </div>
         </div>
-    @include('callcenter.committees.partials.table')
+
+        @include('callcenter.committees.partials.table')
     </div>
 @endsection
