d850e563a5143494d023ea947fbdd06000499687
Orlando Vinícius Valença Ferreira da Silva <orlando.vinicius1987@gmail.com>
2018-09-20 19:01:07 +0000

    Acertos na busca

---
+++ app/Http/Controllers/Api/CommitteesSearch.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Data\Repositories\People as PeopleRepository;
+
+class Search extends Controller
+{
+    public function execute()
+    {
+        return app(PeopleRepository::class)->searchByEverything(
+            request()->get('search')
+        );
+    }
+}
