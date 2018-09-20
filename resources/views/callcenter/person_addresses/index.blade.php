1a13b09c9ff4b008562c9e8ca0d30ff32f1b9c33
Breno Trengrouse <breno@Ogum.local>
2018-09-20 15:06:19 -0300

    styling cards - people page

--- resources/views/callcenter/person_addresses/index.blade.php
+++ resources/views/callcenter/person_addresses/index.blade.php
@@ -1,19 +1,27 @@
-<div class="mt-4">
-    <div class="row align-items-center">
-        <div class="col-4">
-            <h5>
-                <i class="fas fa-map-marked-alt"></i> Endereços
-            </h5>
-        </div>
-        <div class="col-8 text-right">
-            <a id="buttonNovoEndereco"
-               href="{{ route('people_addresses.create',['person_id' => $person->id]) }}"
-               class="btn btn-primary btn-sm pull-right btn-depth"
-            >
-                <i class="fa fa-plus"></i> Novo Endereço
-            </a>
+<div class="card mt-4">
+    <div class="card-header">
+        <div class="row align-items-center">
+            <div class="col-4">
+                <h5>
+                    <i class="fas fa-map-marked-alt"></i> Endereços
+                </h5>
+            </div>
+            <div class="col-8 text-right">
+                <a id="buttonNovoEndereco"
+                   href="{{ route('people_addresses.create',['person_id' => $person->id]) }}"
+                   class="btn btn-primary btn-sm pull-right btn-depth"
+                >
+                    <i class="fa fa-plus"></i> Novo Endereço
+                </a>
+            </div>
         </div>
     </div>
 
-    @include('callcenter.person_addresses.partials.table')
+    <div class="card-body">
+        @include('callcenter.person_addresses.partials.table')
+    </div>
+
+
 </div>
+
+
