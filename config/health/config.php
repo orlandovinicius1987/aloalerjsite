0e99abd86bfe7469b4d406eac0e902379c65525b
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-09-20 18:10:31 -0300

    Configure notifications

--- config/health/config.php
+++ config/health/config.php
@@ -6,7 +6,7 @@ return [
     'resources' => [
         'path' => config_path('health/resources'),
 
-        'enabled' => PragmaRX\Health\Support\Constants::RESOURCES_ENABLED_ALL,
+        'enabled' => ['DebugMode'], //PragmaRX\Health\Support\Constants::RESOURCES_ENABLED_ALL,
     ],
 
     'sort_by' => 'slug',
@@ -27,5 +27,45 @@ return [
         ],
 
         'max_records' => 30,
+    ],
+
+    'notifications' => [
+        'enabled' => true,
+
+        'notify_on' => [
+            'panel' => false,
+            'check' => true,
+            'string' => true,
+            'resource' => false,
+        ],
+
+        'action-title' => 'View App Health',
+
+        'action_message' =>
+            "The '%s' service is in trouble and needs attention%s",
+
+        'from' => [
+            'name' => 'Laravel Health Checker',
+
+            'address' => 'healthchecker@mydomain.com',
+
+            'icon_emoji' => ':anger:',
+        ],
+
+        'scheduler' => [
+            'enabled' => true,
+
+            'frequency' => 'everyMinute', // most methods on -- https://laravel.com/docs/5.3/scheduling#defining-schedules
+        ],
+
+        'users' => [
+            'model' => App\Data\Models\User::class,
+
+            'emails' => ['admin@mydomain.com'],
+        ],
+
+        'channels' => ['mail', 'slack'], // mail, slack
+
+        'notifier' => 'PragmaRX\Health\Notifications',
     ],
 ];
