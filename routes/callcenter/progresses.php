17853381fc7431b0499c70f007347e669a218f49
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-09-21 12:26:31 -0300

    Refactor send notifications

--- routes/callcenter/progresses.php
+++ routes/callcenter/progresses.php
@@ -1,13 +1,18 @@
 <?php
+
 Route::group(['prefix' => 'progresses'], function () {
     Route::get('/create/{record_id}', 'Progresses@create')->name(
         'progresses.create'
     );
 
+    Route::get('/notify/{id}', 'Progresses@notify')->name('progresses.notify');
+
     Route::post('/', 'Progresses@store')->name('progresses.store');
+
     Route::post('/finish', 'Progresses@storeAndFinish')->name(
         'progresses.storeAndFinish'
     );
+
     Route::post('/open', 'Progresses@storeAndOpen')->name(
         'progresses.storeAndOpen'
     );
