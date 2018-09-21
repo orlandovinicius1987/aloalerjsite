28c0f361a32ba5bb6e07aab084087f11d9c7c35f
Breno Trengrouse <breno@Ogum.local>
2018-09-21 17:02:11 -0300

    styling table -+ change color link - link underline +tr table hover color

--- resources/views/callcenter/committees/index.blade.php
+++ resources/views/callcenter/committees/index.blade.php
@@ -1,6 +1,99 @@
 @extends('layouts.app')
 
 @section('content')
+
+
+
+
+
+
+    <div class="row">
+
+        <div class="col-1 bg-colored-blue-darker100 square">
+
+        </div>
+
+        <div class="col-1 bg-colored-blue-darker75 square">
+
+        </div>
+
+        <div class="col-1 bg-colored-blue-darker50 square">
+
+        </div>
+
+        <div class="col-1 bg-colored-blue-darker25 square">
+
+        </div>
+
+        <div class="col-1 bg-colored-blue-lighter95 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter90 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter85 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter80 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter75 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter70 square">
+
+        </div>
+
+        <div class="col-1 bg-colored-blue-lighter65 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter60 square">
+
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="col-1 bg-colored-blue-lighter55 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter50 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter45 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter40 square">
+
+        </div>
+
+        <div class="col-1 bg-colored-blue-lighter35 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter30 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter25 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter20 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter15 square">
+
+        </div>
+        <div class="col-1 bg-colored-blue-lighter10 square">
+
+        </div>
+
+
+
+
+
+    </div>
+
+
     <div class="card mt-4" id="committees-search">
         <div class="card-header">
             <div class="row align-items-center">
