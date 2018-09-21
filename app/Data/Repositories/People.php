fde56c502811b41041b45ef18d42f0ca7dbce8fd
Orlando Vinícius Valença Ferreira da Silva <orlando.vinicius1987@gmail.com>
2018-09-21 14:59:23 +0000

    Correção de linhas apagadas

--- app/Data/Repositories/People.php
+++ app/Data/Repositories/People.php
@@ -1,15 +1,19 @@
 <?php
+
 namespace App\Data\Repositories;
 
 use Validator;
 use App\Data\Models\Person;
+
 class People extends Base
 {
     const RECORDS_COUNT_LIMIT = 20;
+
     /**
      * @var $model
      */
     protected $model = Person::class;
+
     private function addExtraInfo($people)
     {
         return $people;
@@ -18,27 +22,33 @@ class People extends Base
                 $record->protocol_formatted = $record->presenter()
                     ->protocol_formatted;
             });
+
             return $person;
         });
     }
+
     private function emptyResponse($search = '')
     {
         return $this->response($search, [], 0, null);
     }
+
     protected function error($count, $messages)
     {
         return $this->response(null, null, $count, $messages);
     }
+
     protected function getBaseQuery()
     {
         return $this->model::with(['contacts', 'addresses', 'records'])->take(
             static::RECORDS_COUNT_LIMIT + 1
         );
     }
+
     private function isNumeric($string)
     {
         return is_numeric(remove_punctuation($string));
     }
+
     protected function response($string, $data, $count = 0, $messages = null)
     {
         return [
@@ -50,9 +60,11 @@ class People extends Base
             'is_numeric' => $this->isNumeric($string),
         ];
     }
+
     protected function searchByProtocolNumber($string)
     {
         $record = app(Records::class)->findByProtocol($string);
+
         if ($record) {
             $query = $this->model::with(['contacts', 'addresses'])
                 ->with([
@@ -62,60 +74,78 @@ class People extends Base
                 ])
                 ->take(static::RECORDS_COUNT_LIMIT + 1)
                 ->where('id', $record->person_id);
+
             return $this->response($string, $query->get(), $query->count());
         }
+
         return $this->emptyResponse();
     }
+
     protected function searchByCpf($string)
     {
         if (!$this->validCpfCnpj($string)) {
             return $this->emptyResponse($string);
         }
+
         $query = $this->getBaseQuery()->where(
             'cpf_cnpj',
             only_numbers($string)
         );
+
         return $this->response(
             $string,
             $this->addExtraInfo($query->get()),
             $query->count()
         );
     }
+
     protected function searchByName($string)
     {
         $query = $this->getBaseQuery()->whereRaw(
             "unaccent(name) ILIKE '%'||unaccent('{$string}')||'%' "
         );
+
         if ($query->count() > static::RECORDS_COUNT_LIMIT) {
             return $this->error(
                 $query->count(),
                 'Busca resultou em mais de 20 registros'
             );
         }
+
         return $this->response($string, $query->get(), $query->count());
     }
+
     public function searchByEverything($search)
     {
         if (empty(trim($search = $search['search']))) {
             return $this->emptyResponse();
         }
+
         if ($this->isNumeric($search)) {
             $search = remove_punctuation($search);
+
             $response = $this->searchByCpf($search);
+
             if ($response['success'] && $response['count'] > 0) {
                 return $response;
             }
+
             $protocol = $this->searchByProtocolNumber($search);
+
             $response['errors'] = $response['errors'] ?: $protocol['errors'];
+
             if (!is_null($protocol['data'])) {
                 $response['data'] = coollect($response['data'])->merge(
                     $protocol['data']
                 );
             }
+
             return $response;
         }
+
         return $this->searchByName($search);
     }
+
     public function validCpfCnpj($string)
     {
         return Validator::make(
