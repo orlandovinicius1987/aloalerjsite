3f3758668c63912759a8f582555a031d32c6381e
Alexandre Ferreira <alexandreee@gmail.com>
2018-09-24 16:19:01 -0300

    Remover formatação de protocolo e telefone do banco de dados #377
    Remover formatação de protocolo e telefone do banco de dados #377

---
+++ database/migrations/2018_09_24_160000_convert_protocol_and_phone_only_number.php
@@ -0,0 +1,46 @@
+<?php
+
+use App\Data\Models\Area;
+use App\Data\Models\ContactType;
+use App\Data\Models\Origin;
+use App\Data\Models\PersonContact;
+use App\Data\Models\ProgressType;
+use App\Data\Models\Record;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ConvertProtocolAndPhoneOnlyNumber extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        PersonContact::whereIn(
+            'contact_type_id',
+            ContactType::whereIn('code', ['mobile', 'whatsapp', 'phone'])
+                ->get()
+                ->pluck('id')
+        )->each(function ($c) {
+            $c->contact = only_numbers($c->contact);
+            $c->save();
+        });
+
+        Record::all()->each(function ($p) {
+            $p->protocol = only_numbers($p->protocol);
+            $p->save();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+    }
+}
