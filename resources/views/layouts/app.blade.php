855088e15390f3e291ca4481a3db62d02ace2f55
Antonio Carlos Ribeiro <acr@antoniocarlosribeiro.com>
2018-10-03 20:10:31 -0300

    Center login

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,125 +29,122 @@
         </script>
     </head>
     <body>
-        <div id="app">
-            <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="{{ url('/callcenter') }}">
+        <nav class="navbar navbar-expand-md navbar-light navbar-laravel">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="{{ url('/callcenter') }}">
 {{--                        <img src="/templates/mv/svg/logo-alerj-monocromatica.svg"
-                             class="alerj-logo img-responsive  d-none d-xl-block">
+                         class="alerj-logo img-responsive  d-none d-xl-block">
 
-                        <img src="/templates/mv/svg/logo-alo-alerj-nova.svg"
-                             class="alolerj-logo-home img-responsive"
-                             alt="AloAlerj"> - Call Center--}}
+                    <img src="/templates/mv/svg/logo-alo-alerj-nova.svg"
+                         class="alolerj-logo-home img-responsive"
+                         alt="AloAlerj"> - Call Center--}}
 
-                        <img src="/templates/mv/svg/logo-alo-alerj-callcenter.svg"
-                             class="alolerj-logo-home img-responsive"
-                             alt="AloAlerj - Callcenter">
+                    <img src="/templates/mv/svg/logo-alo-alerj-callcenter.svg"
+                         class="alolerj-logo-home img-responsive"
+                         alt="AloAlerj - Callcenter">
 
-                    </a>
+                </a>
 
-                    <button class="navbar-toggler" type="button" data-toggle="collapse"
-                            data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
-                            aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
+                <button class="navbar-toggler" type="button" data-toggle="collapse"
+                        data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                        aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
 
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <!-- Left Side Of Navbar -->
-                        <ul class="navbar-nav mr-auto">
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <!-- Left Side Of Navbar -->
+                    <ul class="navbar-nav mr-auto">
 
-                        </ul>
+                    </ul>
 
-                        <!-- Right Side Of Navbar -->
-                        <ul class="navbar-nav ml-auto">
-                            <!-- Authentication Links -->
-                            @guest
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-out-alt"></i> Entrar </a>
-                                </li>
-                            @else
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{url('/callcenter') }}"><i class="fas fa-search"></i> Pesquisar </a>
-                                </li>
+                    <!-- Right Side Of Navbar -->
+                    <ul class="navbar-nav ml-auto">
+                        <!-- Authentication Links -->
+                        @guest
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{ route('login') }}"><i class="fas fa-sign-out-alt"></i> Entrar </a>
+                            </li>
+                        @else
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{url('/callcenter') }}"><i class="fas fa-search"></i> Pesquisar </a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{route('committees.index') }}"><i class="fas fa-layer-group"></i> Comissões </a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('committees.index') }}"><i class="fas fa-layer-group"></i> Comissões </a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="{{route('records.nonResolved') }}"><i class="fas fa-times-circle"></i> Não Resolvidos </a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="{{route('records.nonResolved') }}"><i class="fas fa-times-circle"></i> Não Resolvidos </a>
+                            </li>
 
-                                <li class="nav-item">
-                                    <a class="nav-link" href="/"><i class="fas fa-headphones-alt"></i> Site do Alô Alerj</a>
-                                </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="/"><i class="fas fa-headphones-alt"></i> Site do Alô Alerj</a>
+                            </li>
 
-                                <li class="nav-item dropdown">
-                                    <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
-                                       data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                                        <i class="fas fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
+                            <li class="nav-item dropdown">
+                                <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button"
+                                   data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                                    <i class="fas fa-user"></i> {{ Auth::user()->name }} <span class="caret"></span>
+                                </a>
+
+                                <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                                    <a class="dropdown-item" href="{{ route('logout') }}"
+                                       onclick="event.preventDefault();
+                                                 document.getElementById('logout-form').submit();">
+                                        Sair
                                     </a>
 
-                                    <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                                        <a class="dropdown-item" href="{{ route('logout') }}"
-                                           onclick="event.preventDefault();
-                                                     document.getElementById('logout-form').submit();">
-                                            Sair
-                                        </a>
-
-                                        <form id="logout-form" action="{{ route('logout') }}" method="POST"
-                                              style="display: none;">
-                                            @csrf
-                                        </form>
-                                    </div>
-                                </li>
-                            @endguest
-                        </ul>
-                    </div>
+                                    <form id="logout-form" action="{{ route('logout') }}" method="POST"
+                                          style="display: none;">
+                                        @csrf
+                                    </form>
+                                </div>
+                            </li>
+                        @endguest
+                    </ul>
                 </div>
-            </nav>
-
-
-            <div class="container-fluid">
-
-                <div class="row">
-                    <div class="col-md-12">
-                        @if (isset($errors) && $errors->any())
-                            <div class="alert alert-danger">
-                                <ul>
-                                    @foreach ($errors->all() as $error)
-                                        <li>{{ $error }}</li>
-                                    @endforeach
-                                </ul>
-                            </div>
-                        @endif
-                    </div>
-                </div>
-
             </div>
+        </nav>
 
 
-            @include('partials.alerts')
-
-            <div id="@yield('vue-app-name')">
-                @if (View::hasSection('heading'))
-                    <div class="container-fluid section-heading-bg">
-                        <div class="row justify-content-center">
-                            <div class="col-md-10">
-                                @section('heading') @show
-                            </div>
+        <div class="container-fluid">
+            <div class="row">
+                <div class="col-md-12">
+                    @if (isset($errors) && $errors->any())
+                        <div class="alert alert-danger">
+                            <ul>
+                                @foreach ($errors->all() as $error)
+                                    <li>{{ $error }}</li>
+                                @endforeach
+                            </ul>
                         </div>
-                    </div>
-                @endif
+                    @endif
+                </div>
+            </div>
+        </div>
 
-                <div class="container-fluid">
+        @include('partials.alerts')
+
+        <div id="@yield('vue-app-name')">
+            @if (View::hasSection('heading'))
+                <div class="container-fluid section-heading-bg">
                     <div class="row justify-content-center">
                         <div class="col-md-10">
-                            @yield('content')
+                            @section('heading') @show
                         </div>
+                    </div>
+                </div>
+            @endif
+
+            <div class="container-fluid">
+                <div class="row justify-content-center">
+                    <div class="col-md-10">
+                        @yield('content')
                     </div>
                 </div>
             </div>
         </div>
+
+        @yield('content-login')
     </body>
 </html>
