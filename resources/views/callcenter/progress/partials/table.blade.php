a485daf284a7c0d7f5d13c1a57809c0f309473fa
Breno Trengrouse <brenot@gmail.com>
2018-10-16 21:37:33 +0000

    Responsive Fixes - css fixes

--- resources/views/callcenter/progress/partials/table.blade.php
+++ resources/views/callcenter/progress/partials/table.blade.php
@@ -1,4 +1,4 @@
-    <div class="card-body" id="vue-progress">
+    <div class="card-body d-none d-sm-block" id="vue-progress">
 
         <table id="progressesTable" class="table table-striped table-hover table-progress" cellspacing="0" width="100%">
             <thead>
@@ -61,3 +61,42 @@
 
         {{ $progresses instanceof \Illuminate\Contracts\Pagination\Paginator ? $progresses->links() : '' }}
     </div>
+
+
+    <!-------------------- Start of MOBILE VERSION -------------------->
+
+    <div class="card-body d-block d-sm-none" id="vue-progress">
+        @forelse ($progresses as $progress)
+        <div class="mobile-tables"  v-on:click='detail("{{$progress->link}}")' style="cursor: pointer;">
+
+            <div class="contact-line"><span class="mobile-label">Tipo de Andamento :</span> {{ $progress->progressType->name ?? '' }}</div>
+            <div class="contact-line"><span class="mobile-label">Origem :</span> {{ $progress->origin->name ?? '' }}</div>
+            <div class="contact-line"><span class="mobile-label">Área :</span> {{ $progress->area->name ?? '' }}</div>
+            <div class="contact-line"><span class="mobile-label">Solicitação :</span> {{ $progress->original }}</div>
+            <div class="contact-line"><span class="mobile-label">Finalizador :</span>
+                @if ($progress->record->resolve_progress_id == $progress->id)
+                    @if($progress->finalize)
+                        <h5><span class="badge badge-success">Sim</span></h5>
+                    @endif
+                @else
+                    <span class="label-group"><span class="label label-danger"><i class="fas fa-times-circle"></i></span><span class="label label-danger ng-binding">Não</span>
+                    {{--<h5><span class="badge badge-danger">Não</span></h5>--}}
+                @endif
+            </div>
+            <div class="contact-line">
+                <span class="mobile-label">Notificação :</span>@if ($progress->email_sent_at)
+                    <span class="label-group"><span class="label label-primary"><i class="far fa-envelope"></i></span><span class="label label-primary ng-binding"> E-Mail </span>
+                @endif
+            </div>
+            <div class="contact-line"><span class="mobile-label">Criado em :</span> {{ $progress->created_at_formatted ?? '' }}</div>
+
+
+        </div>
+        @empty
+            <p>Nenhum andamento encontrado.</p>
+        @endforelse
+        {{ $progresses instanceof \Illuminate\Contracts\Pagination\Paginator ? $progresses->links() : '' }}
+
+    </div>
+
+    <!-------------------- Start of MOBILE VERSION -------------------->
