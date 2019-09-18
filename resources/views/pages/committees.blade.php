c9ab8bbe3c3cb03393531559524e6e30b34a5fa5
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2019-09-18 17:40:05 +0000

    Erro em Produção

---
+++ resources/views/pages/committees.blade.php
@@ -0,0 +1,17 @@
+@extends('layouts.master')
+
+@section('content-main')
+
+    <div class="page-name">
+        <h1 class="nome-pagina">Comissões</h1>
+    </div>
+    <div class="texto-pages">
+        <p>O Alô Alerj é um canal de comunicação entre você e a Assembleia Legislativa do Estado do Rio de Janeiro (Alerj). Aqui você pode registrar reclamações, solicitações e sugestões sobre os temas tratados nas nossas comissões:</p>
+
+        <div class="comissoes">
+            @foreach($committees as $committee)
+                <p><a href="/comissoes/{{$committee->slug}}">{{$committee->name}}</a></p>
+            @endforeach
+        </div>
+    </div>
+@stop
