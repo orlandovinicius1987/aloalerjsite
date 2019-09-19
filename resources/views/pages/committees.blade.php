4673c27c5a27ad41e232da9b3cbf0642b1740276
BMasquio <brunomasquio@gmail.com>
2019-09-19 15:28:22 +0000

    Fix committees

--- resources/views/pages/committees.blade.php
+++ resources/views/pages/committees.blade.php
@@ -2,6 +2,14 @@
 
 @section('content-main')
 
+
+
+
+
+
+
+
+
     <div class="page-name">
         <h1 class="nome-pagina">Comissões</h1>
     </div>
@@ -9,9 +17,22 @@
         <p>O Alô Alerj é um canal de comunicação entre você e a Assembleia Legislativa do Estado do Rio de Janeiro (Alerj). Aqui você pode registrar reclamações, solicitações e sugestões sobre os temas tratados nas nossas comissões:</p>
 
         <div class="comissoes">
-            @foreach($committees as $committee)
-                <p><a href="/comissoes/{{$committee->slug}}">{{$committee->name}}</a></p>
-            @endforeach
+            <p><a href="/comissoes/disquecrianca">Disque Criança e Adolescente</a></p>
+            <p><a href="/comissoes/disqueidoso">Disque Idoso</a></p>
+            <p><a href="/comissoes/preconceitos">Combate às Discriminações e Preconceitos</a></p>
+            <p><a href="/comissoes/defesadoconsumidor">Defesa do Consumidor</a></p>
+            <p><a href="/comissoes/sosmulher">Defesa dos Direitos da Mulher</a></p>
+            <p><a href="/comissoes/direitoshumanos">Defesa dos Direitos Humanos e Cidadania</a></p>
+            <p><a href="/comissoes/meioambiente">Defesa Meio Ambiente</a></p>
+            <p><a href="/comissoes/educacao">Educação</a></p>
+            <p><a href="/comissoes/pessoadeficiente">Pessoa com Deficiência</a></p>
+            <p><a href="/comissoes/pirataria">Prevenção e Combate a Pirataria</a></p>
+            <p><a href="/comissoes/animais">Proteção ao Direito dos Animais</a></p>
+            <p><a href="/comissoes/saneamentoambiental">Saneamento Ambiental</a></p>
+            <p><a href="/comissoes/segurancaalimentar">Segurança Alimentar</a></p>
+            <p><a href="/comissoes/segurancapublica">Segurança Pública e Assuntos de Polícia</a></p>
+            <p><a href="/comissoes/trabalho">Trabalho, Legislação Social e Seguridade Social</a></p>
+            <p><a href="/comissoes/tributacao">Tributação e Controle da Arrecadação</a></p>
         </div>
     </div>
 @stop
