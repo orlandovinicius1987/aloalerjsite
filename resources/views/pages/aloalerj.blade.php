bb93477d321af5271fc43f65a515a94c05c8c147
BMasquio <brunomasquio@gmail.com>
2019-10-29 10:18:31 -0300

    Changed presentation text #496

--- resources/views/pages/aloalerj.blade.php
+++ resources/views/pages/aloalerj.blade.php
@@ -7,9 +7,8 @@
 
     <div class="texto-pages">
         <p>O Alô Alerj é o canal de comunicação entre o cidadão e a Assembléia Legislativa do Estado do Rio de Janeiro (Alerj).</p>
-        <p>Possuímos operadores preparados com o intuito de esclarecer os temas/assuntos que são tratados nas Comissões e no Plenário, como também o funcionamento desta Casa Legislativa.</p>
-        <p>Para agilizar seu atendimento tenha em mãos os seus dados pessoais. O registro de sua demanda gera um número de protocolo, que é fornecido por nossos atendentes. Não esqueça de anotá-lo.</p>
-        <p>Nosso atendimento é de 2ª feira à 6ª feira de 09h às 18h.</p>
-
+        <p>Possuímos operadores preparados com o intuito de esclarecer não só os temas e assuntos que são tratados nas Comissões e no Plenário, mas também o funcionamento desta Casa Legislativa.</p>
+        <p>Para agilizar seu atendimento, tenha em mãos os seus dados pessoais. O registro de sua demanda gera um número de protocolo, que é fornecido por nossos atendentes. Não esqueça de anotá-lo.</p>
+        <p>Nosso atendimento é de 2ª feira a 6ª feira das 09h às 18h.</p>
     </div>
 @stop
