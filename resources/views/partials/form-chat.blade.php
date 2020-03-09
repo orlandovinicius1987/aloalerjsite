9e168079a79d635eb2a3473cff40a437ef5e4aaf
orlandovinicius1987 <orlando.vinicius1987@gmail.com>
2020-03-09 15:31:32 -0300

    Issue: #554

--- resources/views/partials/form-chat.blade.php
+++ resources/views/partials/form-chat.blade.php
@@ -1,33 +1,30 @@
-{{--<div class="boxshadow" id="vue-chat">--}}
-{{--    <div v-show="chatOnline" style="display: none">--}}
-{{--        @include('partials.form-chat-online')--}}
-{{--    </div>--}}
+@if((strtotime(date('H:i')) > strtotime('09:00') && strtotime(date('H:i')) < strtotime(date('18:00'))))
 
-{{--    <div v-show="! chatOnline">--}}
-{{--        @include('partials.form-chat-offline')--}}
-{{--    </div>--}}
-{{--</div>--}}
+    <script type='text/javascript' data-cfasync='false'>
 
-<script type='text/javascript' data-cfasync='false'>
-    window.purechatApi = { l: [], t: [], on:
-            function () {
-                this.l.push(arguments);
-            }};
-    (function () {
-        var done = false;
-        var script = document.createElement('script');
-        script.async = true;
-        script.type = 'text/javascript';
-        script.src = '{{env('CHAT_CLIENT_URL')}}';
-        document.getElementsByTagName('HEAD').item(0).appendChild(script);
-        script.onreadystatechange = script.onload = function (e) {
-            if (!done && (!this.readyState || this.readyState == 'loaded'
-                || this.readyState == 'complete')) {
+        window.purechatApi = { l: [], t: [], on:
+                function () {
+                    this.l.push(arguments);
+                }};
 
-                var w = new PCWidget({c: '{{ env('CHAT_CLIENT_ID') }}', f: true });
-                done = true;
-            }
-        };
-    })
-    ();
-</script>
+        (function () {
+            var done = false;
+            var script = document.createElement('script');
+            script.async = true;
+            script.type = 'text/javascript';
+            script.src = '{{env('CHAT_CLIENT_URL')}}';
+            document.getElementsByTagName('HEAD').item(0).appendChild(script);
+            script.onreadystatechange = script.onload = function (e) {
+                if (!done && (!this.readyState || this.readyState == 'loaded'
+                    || this.readyState == 'complete')) {
+
+                    var w = new PCWidget({c: '{{ env('CHAT_CLIENT_ID') }}', f: true });
+                    done = true;
+                }
+            };
+        })();
+    </script>
+
+@else
+    @include('partials.form-chat-offline')
+@endif
