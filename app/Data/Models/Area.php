5b4c2ce74b1a4be7d1747797ef69f4718bb75c52
BMasquio <brunomasquio@gmail.com>
2020-08-05 18:18:08 +0000

    Adicionar is_active para Areas - Issue: #625

--- app/Data/Models/Area.php
+++ app/Data/Models/Area.php
@@ -1,10 +1,14 @@
 <?php
 namespace App\Data\Models;
 
+use App\Data\Traits\Activable;
+
 class Area extends BaseModel
 {
+    use Activable;
+
     /**
      * @var array
      */
-    protected $fillable = ['name'];
+    protected $fillable = ['name', 'is_active'];
 }
