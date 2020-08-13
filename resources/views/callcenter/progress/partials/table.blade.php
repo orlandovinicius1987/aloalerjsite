929bc049e0e6ec66e7e79ffff73c6ec0c252c161
BMasquio <brunomasquio@gmail.com>
2020-08-13 17:58:59 +0000

    Colocar contatos, solicitação e opção de imprimir na busca avançada - Issue #642

--- resources/views/callcenter/progress/partials/table.blade.php
+++ resources/views/callcenter/progress/partials/table.blade.php
@@ -24,7 +24,7 @@
                     </td>
 
 
-                    <td>
+                    <td style="word-wrap: break-word; width: 40%; max-width: 20px;">
                         {{ $progress->original }}
                     </td>
 
