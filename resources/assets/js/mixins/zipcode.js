9704943033c78aa7ed19ead422d0b01f6b858dbe
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2020-09-15 14:10:56 +0000

    Revisão de código

---
+++ resources/assets/js/mixins/zipcode.js
@@ -0,0 +1,99 @@
+export default {
+    data: {
+        refreshing: false,
+
+        filler: false,
+
+        typeTimeout: null,
+
+
+        tables: {
+            addresses: [],
+        },
+
+        form: {
+            zipcode: null,
+            street: null,
+            neighbourhood: null,
+            city: null,
+            state: null,
+        },
+    },
+
+    methods: {
+        refresh() {
+            let $this = this
+
+            $this.refreshing = true
+
+            console.log('form.zipcode: '+ this.form.zipcode)
+            axios
+                .get('/api/v1/zipcode/' + this.form.zipcode, {
+                    params: {
+                        api_token: laravel.api_token,
+                    },
+                })
+                .then(function(response) {
+                    $this.tables.addresses = response.data
+                    console.log(response.data)
+                    if (response.data.addresses[0].street_name) {
+                        $this.form.zipcode = response.data.addresses[0].zip
+                        $this.form.street =
+                            response.data.addresses[0].street_name
+                        $this.form.neighbourhood =
+                            response.data.addresses[0].neighborhood
+                        $this.form.city = response.data.addresses[0].city
+                        $this.form.state =
+                            response.data.addresses[0].state_id
+                        $this.form.country = 'Brasil'
+                        document.getElementById('number').focus()
+                    }
+
+                    $this.refreshing = false
+                })
+                .catch(function(error) {
+                    console.log(error)
+
+                    $this.tables.addresses = []
+
+                    $this.refreshing = false
+                })
+        },
+
+        typeKeyUp() {
+            clearTimeout(this.timeout)
+
+            const $this = this
+
+            this.timeout = setTimeout(function() {
+                $this.refresh()
+            }, 500)
+        },
+
+        isNumber: function(evt) {
+            evt = evt ? evt : window.event
+            charCode = evt.which ? evt.which : evt.keyCode
+            if (
+                charCode > 31 &&
+                (charCode < 48 || charCode > 57) &&
+                charCode !== 46
+            ) {
+                evt.preventDefault()
+            } else {
+                return true
+            }
+        },
+    },
+
+    computed: {
+        isShowing() {
+            return this.mode === 'show'
+        },
+        isEditing() {
+            return this.mode === 'edit'
+        },
+        isCreating() {
+            return this.mode === 'create'
+        },
+      }
+}
