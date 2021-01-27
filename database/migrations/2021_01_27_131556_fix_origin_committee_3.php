798e0dd3aa92e5788c38533617fdcb90958143cd
Orlando Vinícius <orlando.vinicius1987@gmail.com>
2021-01-27 16:34:56 +0000

    Issue: #775

---
+++ database/migrations/2021_01_27_131556_fix_origin_committee_3.php
@@ -0,0 +1,82 @@
+<?php
+
+use App\Data\Models\Audit as AuditModel;
+use App\Data\Models\Committee as CommitteeModel;
+use Illuminate\Database\Migrations\Migration;
+use App\Data\Models\Record as RecordModel;
+use Carbon\Carbon;
+
+class FixOriginCommittee3 extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        foreach (
+                    RecordModel::whereDate(
+                        'created_at',
+                        '>=',
+                        Carbon::createFromFormat('Y-m-d', '2020-08-01')
+                    )
+                        ->whereDate(
+                            'created_at',
+                            '<=',
+                            Carbon::createFromFormat('Y-m-d', '2020-10-01')
+                        )
+                        ->cursor()
+                    as $record
+                ) {
+                    $progress = $record->firstProgress();
+
+                    $audit = AuditModel::where('auditable_id', $progress->id)
+                        ->where('event', 'created')
+                        ->where('auditable_type', 'App\Data\Models\Progress')
+                        ->first();
+
+                    if ($audit && ($userId = $audit->user_id)) {
+                        $oldCommitteeId = $progress->created_by_committee_id;
+                        
+
+                        if ($creator = $progress->creator) {
+                            $newCommitteeId = $progress->created_by_committee_id = $creator->originCommittee()->id;
+                        } else {
+                            $newCommitteeId = $progress->created_by_committee_id = CommitteeModel::where(
+                                'slug',
+                                'alo-alerj'
+                            )->first()->id;
+                        }
+
+                        if ($oldCommitteeId != $newCommitteeId) {
+                            dump(
+                                "Alterando o progress {$progress->id} da comissão {$oldCommitteeId} para a comissão {$newCommitteeId}"
+                            );
+                        } else {
+                            dump(
+                                "Mantendo o progress {$progress->id} na comissão {$progress->created_by_committee_id}"
+                            );
+                        }
+
+                        $progress->save();
+                    } else {
+                        dump(
+                            "Não foi possível resgatar o audit do progress {$progress->id}. Audit encontrado = " .
+                                $audit->id ??
+                                'null'
+                        );
+                    }
+                }
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        //
+    }
+}
