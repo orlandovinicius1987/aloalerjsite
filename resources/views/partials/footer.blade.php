b26a89cc0357eeb4e4f0ea1ea8a932fd0be43c57
Breno Trengrouse <breno@gmail.com>
2021-02-08 14:45:38 -0300

    Font Awesome Issue to Bruno

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -1,24 +1,58 @@
 <!-- Footer -->
 <section id="footer">
     <div class="container">
-        <div class="row mt-5">
 
-            <div class="col-12 col-sm-4 col-md-3 ">
+{{--
+
+        <div class="row">
+            <div class="col-12 col-sm-4 col-md-3 offset-md-1">
                 <img src="/templates/2021/images/ALERJ_NOVO_horizontal-branco.png" class="img-fluid">
+            </div>
+            <div class="col-12 col-sm-4 col-md-7 text-white">
+                --}}
+{{--<p class="titulo">PALÁCIO TIRADENTES</p>--}}{{--
 
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-12 mt-2 mt-sm-5 text-white text-center">
-                        <p class="titulo">PALÁCIO TIRADENTES</p>
-                        <p>Rua Primeiro de março, s/n <br> Praça XV - Rio de Janeiro<br><span>CEP</span> 20010-090 &nbsp;&nbsp; <span>Telefone</span> +55 (21) 2588-1000 &nbsp;&nbsp; </p>
+                <p>Rua Primeiro de março, s/n  |  Praça XV - Rio de Janeiro<br><span>CEP</span> 20010-090 &nbsp;&nbsp; <span>Telefone</span> +55 (21) 2588-1000 &nbsp;&nbsp; </p>
+                <hr class="">
+                <ul class="list-unstyled list-inline social">
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-facebook"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-twitter"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-instagram"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-google-plus"></i></a></li>
+                    <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fab fa-envelope"></i></a></li>
+                </ul>
+            </div>
+        </div>
+
+--}}
+
+        <div class="row">
+            <div class="comissoes col-md-12 text-white mt-4">
+{{--                <hr class="mb-3">--}}
+                <h3 class="mb-3">Comissões <i class="fab fa-facebook"></i></h3>
+                <ul class="list-inline">
+                    @foreach($committeeServices as $committeService)
+                        <li><a href="{{ route('services.show', ['id'=>$committeService->id]) }}">{{$committeService->link_caption}}</a></li>
+                    @endforeach
+                </ul>
+                <hr class="mt-5">
+            </div>
+        </div>
 
 
+        <div class="row mt-2">
+            <div class="col-12 col-sm-4 col-md-4 ">
+                <img src="/templates/2021/images/ALERJ_NOVO_horizontal-branco.png" class="img-fluid">
+            </div>
+            <div class="col-12 col-sm-4 col-md-8 text-white">
+                {{--<p class="titulo">PALÁCIO TIRADENTES</p>--}}
+
+                <div class="row mt-4">
+                    <div class="col-6">
+                        <p>Rua Primeiro de março, s/n  |  Praça XV - Rio de Janeiro<br><span>CEP</span> 20010-090 &nbsp;&nbsp; <span>Telefone</span> +55 (21) 2588-1000 &nbsp;&nbsp; </p>
                     </div>
-                    <hr>
-                </div>
-
-                <div class="row">
-                    <div class="col-12 col-sm-12 col-md-12 mt-2 mt-sm-4">
-                        <ul class="list-unstyled list-inline social text-center">
+                    <div class="col-6">
+                        <ul class="list-unstyled list-inline social">
                             <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-facebook"></i></a></li>
                             <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-twitter"></i></a></li>
                             <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02"><i class="fab fa-instagram"></i></a></li>
@@ -26,44 +60,37 @@
                             <li class="list-inline-item"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="fab fa-envelope"></i></a></li>
                         </ul>
                     </div>
-                    <hr>
-                </div>
-
-            </div>
-
-            <div class="col-12 col-sm-8 col-md-9">
-
-                <div class="row">
-
-                    <div class="col-md-12 text-white mb-4">
-                        <h3>Comissões</h3>
-                    </div>
                 </div>
 
                 <div class="row">
-                    <div class="comissoes col-md-12">
-                        <ul class="list-inline text-center" >
-                        @foreach($committeeServices as $committeService)
-                            <li><a href="{{ route('services.show', ['id'=>$committeService->id]) }}">{{$committeService->link_caption}}</a></li>
-                        @endforeach
-                        </ul>
+                    <div class="col-12">
+                        <hr class="mb-4">
+                        <p class="h6">© Todos os direitos reservados. |
+                            <a class="ml-2" href="http://www.alerj.rj.gov.br/" target="_blank">
+                                ALERJ - Assémbleia Legislativa do Estado do Rio de Janeiro
+                            </a>
+                        </p>
+
                     </div>
                 </div>
+
+
             </div>
         </div>
 
-        <div class="row">
-            <div class="col-12 col-sm-12 col-md-12 mt-2 mt-sm-2 text-center text-white">
+
+{{--
+        <div class="row mt-5">
+            <div class="col-12 col-sm-12 col-md-12 text-center text-white">
                 <p class="h6">© Todos os direitos reservados. |
                     <a class="ml-2" href="http://www.alerj.rj.gov.br/" target="_blank">
                         ALERJ - Assémbleia Legislativa do Estado do Rio de Janeiro
                     </a>
                 </p>
-
             </div>
             <hr>
         </div>
-
+--}}
 
     </div>
 </section>
--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,30 +1,63 @@
 <!-- Header -->
 
+<nav class="navbar navbar-expand-md navbar-dark bg-primary" aria-label="Fourth navbar example">
+    <div class="container-fluid">
+        <a class="navbar-brand logo-alerj" href="#">
+            <img src="/templates/2021/images/ALERJ_NOVO_vertical-branco.png">
+        </a>
+        <a class="navbar-brand logo-aloalerj" href="#">
+            <img src="/templates/2021/svg/logo-alo-alerj-branca-nova.svg">
+        </a>
+
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample04" aria-controls="navbarsExample04" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse navbar-right" id="navbarsExample04">
+            <ul class="navbar-nav me-auto mb-2 mb-md-0 ">
+                <li class="nav-item active">
+                    <a class="nav-link" aria-current="page" href="#">Home</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Link</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
+                </li>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
+                    <ul class="dropdown-menu" aria-labelledby="dropdown04">
+                        <li><a class="dropdown-item" href="#">Action</a></li>
+                        <li><a class="dropdown-item" href="#">Another action</a></li>
+                        <li><a class="dropdown-item" href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+
+            <form>
+                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
+            </form>
+
+        </div>
+    </div>
+</nav>
+
+
+
+{{--
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
     <div class="container-fluid">
 
-        <div class="col-lg-1 logo-alerj">
-            <a class="navbar-brand mr-5" href="#">
-                <img src="/templates/2021/images/ALERJ_NOVO_vertical-branco.png" class="img-fluid">
+            <a class="navbar-brand mr-5 logo-alerj" href="#">
+                <img src="/templates/2021/images/ALERJ_NOVO_vertical-branco.png">
             </a>
-        </div>
-
-
-        <div class="col-lg-1 offset-lg-1">
-            <a class="navbar-brand ml-5" href="/">
-                <img src="/templates/2021/svg/logo-alo-alerj-branca-nova.svg" class="img-fluid">
-            </a>
-        </div>
-
-        <div class="col-lg-8">
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
 
-            <div class="collapse navbar-collapse d-flex justify-content-end" id="navbarNav">
-                <ul class="navbar-nav">
-
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav d-flex justify-content-end">
 
                     <li class="nav-item">
                         <a class="nav-link active" aria-current="page" href="#">Home</a>
@@ -53,14 +86,9 @@
                     <input class="form-control" type="text" placeholder="Número de Protocolo" aria-label="Número de Protocolo">
                 </form>
             </div>
-
-        </div>
-
-
     </div>
 </nav>
-
-
+--}}
 
 
 {{--
