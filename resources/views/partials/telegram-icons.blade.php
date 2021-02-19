1daa3ec287aa1142c5f57b7d0fd96a494b9fd764
Breno Trengrouse <breno@gmail.com>
2021-02-19 13:29:53 -0300

    Body Responsive 2

--- resources/views/partials/telegram-icons.blade.php
+++ resources/views/partials/telegram-icons.blade.php
@@ -1,11 +1,11 @@
 
-    <div class="col-3 offset-3">
+    <div class="col-4 offset-2 col-md-3 offset-md-3">
         <a href="https://wa.me/5521988904742" target="_blank" class="whatsapp">
             <img src="/templates/mv/svg/whatsapp.svg" class="img-responsive" alt="whatsapp">
             <p class="leg-whatsapp">WhatsApp</p>
         </a>
     </div>
-    <div class="col-3">
+    <div class="col-4 col-md-3">
         <a href="https://t.me/aloalerjatendimento" target="_blank" class="telegram">
             <img src="/templates/mv/svg/telegram.svg" class="img-responsive" alt="telegram">
             <p class="leg-telegram">Telegram</p>
