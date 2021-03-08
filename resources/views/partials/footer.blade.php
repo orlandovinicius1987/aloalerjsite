15b722472951291d8f2707cd3267d8131efd7eae
Breno Trengrouse <breno@gmail.com>
2021-03-08 09:52:34 -0300

    alerj new logo

--- resources/views/partials/footer.blade.php
+++ resources/views/partials/footer.blade.php
@@ -14,6 +14,39 @@
             </div>
         </div>
 
+
+        <div class="row d-flex align-items-center bd-highlight mb-3 text-white">
+            <div class="col-12 col-sm-4 col-md-4 ">
+                <a href="//www.alerj.rj.gov.br/" target="_blank">
+                    <img src="/templates/2021/images/ALERJ_NOVO_horizontal-branco.png" class="img-fluid">
+                </a>
+            </div>
+            <div class="col-12 col-sm-4 col-md-8 text-white ">
+                <div class="row mt-4">
+                    <div class="col-lg-6">
+                        <p>Rua Primeiro de março, s/n<br>
+                            Praça XV - Rio de Janeiro<br>
+                            <span>CEP</span> 20010-090 &nbsp;&nbsp; <span class="telefone">+55 (21) 2588-1000</span>
+                        </p>
+                    </div>
+                    <div class="col-lg-6">
+                        <div class="social row">
+                            <div class="col-2"><a href="https://www.facebook.com/assembleiaRJ" target="_blank"><i class="fab fa-facebook"></i></a></div>
+                            <div class="col-2"><a href="https://twitter.com/alerj" target="_blank"><i class="fab fa-twitter"></i></a></div>
+                            <div class="col-2"><a href="https://instagram.com/instalerj/" target="_blank"><i class="fab fa-instagram"></i></a></div>
+                            <div class="col-2"><a href="https://www.youtube.com/user/dcsalerj" target="_blank"><i class="fab fa-youtube"></i></a></div>
+                            <div class="col-2"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="far fa-envelope"></i></a></div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+
+
+
+{{--
         <div class="row mt-2">
             <div class="col-12 col-sm-4 col-md-4 ">
                 <a href="//www.alerj.rj.gov.br/" target="_blank">
@@ -27,19 +60,19 @@
                         <p>Rua Primeiro de março, s/n  |  Praça XV - Rio de Janeiro<br><span>CEP</span> 20010-090 &nbsp;&nbsp; <span>Telefone</span> +55 (21) 2588-1000 &nbsp;&nbsp; </p>
                     </div>
                     <div class="col-lg-6">
-                        <div class="list-unstyled list-inline social row">
-
-                            <li class="list-inline-item col-2"><a href="https://www.facebook.com/assembleiaRJ" target="_blank"><i class="fab fa-facebook"></i></a></li>
-                            <li class="list-inline-item col-2"><a href="https://twitter.com/alerj" target="_blank"><i class="fab fa-twitter"></i></a></li>
-                            <li class="list-inline-item col-2"><a href="https://instagram.com/instalerj/" target="_blank"><i class="fab fa-instagram"></i></a></li>
-                            <li class="list-inline-item col-2"><a href="https://www.youtube.com/user/dcsalerj" target="_blank"><i class="fab fa-youtube"></i></a></li>
-                            <li class="list-inline-item col-2"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="far fa-envelope"></i></a></li>
+                        <div class="social row">
+                            <div class="col-2"><a href="https://www.facebook.com/assembleiaRJ" target="_blank"><i class="fab fa-facebook"></i></a></div>
+                            <div class="col-2"><a href="https://twitter.com/alerj" target="_blank"><i class="fab fa-twitter"></i></a></div>
+                            <div class="col-2"><a href="https://instagram.com/instalerj/" target="_blank"><i class="fab fa-instagram"></i></a></div>
+                            <div class="col-2"><a href="https://www.youtube.com/user/dcsalerj" target="_blank"><i class="fab fa-youtube"></i></a></div>
+                            <div class="col-2"><a href="https://www.fiverr.com/share/qb8D02" target="_blank"><i class="far fa-envelope"></i></a></div>
                         </div>
                     </div>
                 </div>
 
             </div>
         </div>
+--}}
 
 
         <div class="row text-center text-white">
