049f92f767945d74eb71e5823110b1c0e8b7f589
BMasquio <brunomasquio@gmail.com>
2021-03-15 16:44:28 -0300

    Mudança no horário

--- resources/views/partials/form-chat-offline.blade.php
+++ resources/views/partials/form-chat-offline.blade.php
@@ -4,7 +4,7 @@
             <img src="/templates/mv/svg/balao-chat.svg" class="balao-chat offline">
         </div>
 
-        <div class="chat-horario">Nosso horário de atendimento é de 2ª feira à 6ª feira de 10h às 17h.</div>
+        <div class="chat-horario">Nosso horário de atendimento é de 2ª feira à 6ª feira de 10h às 18h.</div>
         <div class="chat-offline-mensagem">Envie uma mensagem para que possamos iniciar o seu atendimento.</div>
     </div>
 
