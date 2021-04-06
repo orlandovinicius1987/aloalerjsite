2f6071735218cea1c0473b5ba4365d50ce916a2b
Orlando Vinicius <orlando.vinicius1987@gmail.com>
2021-04-06 17:00:38 +0000

    Issue: #819

--- app/Console/Commands/unifyPeope.php
+++ app/Console/Commands/unifyPeope.php
@@ -3,6 +3,8 @@
 namespace App\Console\Commands;
 
 use App\Data\Models\Person;
+use App\Data\Models\PersonAddress;
+use App\Data\Models\PersonContact;
 use App\Data\Models\Record as RecordModel;
 use App\Data\Repositories\Users as UsersRepository;
 use Illuminate\Console\Command;
@@ -20,7 +22,7 @@ class unifyPeope extends Command
      *
      * @var string
      */
-    protected $signature = 'aloalerj:unify-People {person_id1} {person_id2}';
+    protected $signature = 'aloalerj:unify-People {person_id_from} {person_id_to}';
 
     /**
      * The console command description.
@@ -46,33 +48,77 @@ class unifyPeope extends Command
      */
     public function handle()
     {
-
         //dd($this->argument('person_id1') . $this->argument('person_id2') );
-        $person_id_old = $this->argument('person_id1');
-        $person_id_new = $this->argument('person_id2');
+        $person_id_from = $this->argument('person_id_from');
+        $person_id_to = $this->argument('person_id_to');
 
-        $personOld = Person::where('id', $person_id_old)->first();
-        $personNew = Person::where('id', $person_id_new)->first();
+        $personFrom = Person::where('id', $person_id_from)->first();
+        $personTo = Person::where('id', $person_id_to)->first();
 
-
-        if($person_id_old && $person_id_new) {
-
-
-            dump('Updating users ' . $personOld->name . ' <- ' . $personNew->name);
+        if ($personFrom && $personTo) {
+            dump('Updating users ' . $personFrom->name . ' -> ' . $personTo->name);
 
             RecordModel::withoutGlobalScopes()
-                ->where('person_id', $personNew->id)
+                ->where('person_id', $personFrom->id)
                 ->get()
-                ->each(function ($record) use ($personOld, $personNew) {
-                    dump(
-                        "Updating record {$record->protocol} from {$personNew->name} to {$personOld->name}"
-                    );
-                    $record->person_id = $personOld->name;
-//                    $record->save();
+                ->each(function ($record) use ($personFrom, $personTo) {
+                    dump("Updating record {$record->protocol} from {$personFrom->name} to {$personTo->name}");
+                    $record->person_id = $personTo->id;
+                    $record->save();
                 });
 
+            PersonContact::where('person_id', $personFrom->id)
+                ->get()
+                ->each(function ($personContact) use ($personFrom, $personTo) {
+                    if (
+                        $personContactTo = PersonContact::where('person_id', $personTo->id)
+                            ->where('contact', $personContact->contact)
+                            ->first()
+                    ) {
+                        dump("Já existe o contato {$personContact->contact} para {$personTo->name} ");
+                        $personContactTo->status = $personFrom->status;
+                        $personContactTo->save();
 
-        }else{
+                        $personContact->delete();
+                    } else {
+                        dump(
+                            "Updating Contact {$personContact->contact} from {$personFrom->name} to {$personTo->name}"
+                        );
+                        $personContact->person_id = $personTo->id;
+                        $personContact->save();
+                    }
+                });
+
+            PersonAddress::where('person_id', $personFrom->id)
+                ->get()
+                ->each(function ($personAddress) use ($personFrom, $personTo) {
+                    if (
+                        $personAddressTo = PersonAddress::where('person_id', $personTo->id)
+                            ->where('zipcode', $personAddress->zipcode)
+                            ->where('number', $personAddress->number)
+                            ->first()
+                    ) {
+                        dump(
+                            "Já existe o endereço {$personAddressTo->zipcode} {$personAddressTo->number} " .
+                                "para {$personTo->name} "
+                        );
+
+                        $personAddressTo->status = $personAddress->status;
+                        $personAddressTo->save();
+
+                        $personAddress->delete();
+                    } else {
+                        dump(
+                            "Updating Address {$personAddress->zipcode} {$personAddress->number}" .
+                                " from {$personFrom->name} to {$personTo->name}"
+                        );
+                        $personAddress->person_id = $personTo->id;
+
+                        $personAddress->save();
+                    }
+                });
+            $personFrom->delete();
+        } else {
             dump('Informe um id válido');
         }
     }
