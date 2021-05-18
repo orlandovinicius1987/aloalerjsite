8021809194a498011edf241bb0466a82d1b49745
Breno Trengrouse <brenot@gmail.com>
2021-05-18 13:06:34 -0300

    Ajustes Responsivos

--- resources/views/callcenter/person_addresses/index.blade.php
+++ resources/views/callcenter/person_addresses/index.blade.php
@@ -1,16 +1,16 @@
 <div class="card mt-4">
     <div class="card-header">
         <div class="row align-items-center">
-            <div class="col-7 col-md-4">
+            <div class="col-6 col-md-8">
                 <h3>
                     <i class="fas fa-map-marked-alt"></i> Endereços
                 </h3>
             </div>
             @if(($person && !$person->is_anonymous))
-                <div class="col-5 col-md-8 text-right">
+                <div class="col-6 col-md-4 text-right">
                     <a id="button-novo-endereco"
                        href="{{ route('people_addresses.create',['person_id' => $person->id]) }}"
-                       class="btn btn-primary btn-sm pull-right"
+                       class="btn btn-primary btn-sm btn-block pull-right"
                         {{($person && $person->is_anonymous) ? 'disabled' : '' }}
                     >
                         <i class="fa fa-plus"></i> Novo Endereço
