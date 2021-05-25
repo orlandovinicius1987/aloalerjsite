e6b8716c74a1ca486f48a4db11ddb842b9300d40
BMasquio <brunomasquio@gmail.com>
2021-05-25 16:21:03 -0300

    Issue #872

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,53 +1,4 @@
 <!-- Header -->
-{{--
-
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary" aria-label="Tenth navbar example">
-    <div class="container-fluid">
-
-        <a class="navbar-brand logo-alerj" href="#">
-            <img src="/templates/2021/images/ALERJ_NOVO_vertical-branco.png">
-        </a>
-        <a class="navbar-brand logo-aloalerj" href="#">
-            <img src="/templates/2021/svg/logo-alo-alerj-branca-nova.svg">
-        </a>
-
-
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample08" aria-controls="navbarsExample08" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-        </button>
-
-        <div class="collapse navbar-collapse justify-content-md-end" id="navbarsExample08">
-            <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" aria-current="page" href="#">Centered nav only</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="#">Link</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown08" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdown08">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
-            </ul>
-
-            <form>
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-            </form>
-
-        </div>
-    </div>
-</nav>
-
---}}
-
-
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary" aria-label="Fourth navbar example">
     <div class="container-fluid">
         <a class="navbar-brand logo-alerj" href="http://www.alerj.rj.gov.br/" target="_blank">
@@ -63,52 +14,24 @@
 
         <div class="collapse navbar-collapse justify-content-md-end" id="navbarsExample08">
             <ul class="navbar-nav">
-                <li class="nav-item active">
-                    <a class="nav-link" aria-current="page" href="/">Home</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/pages/aloalerj">O Alô Alerj</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/pages/committees">Comissões</a>
-                </li>
-                <li class="nav-item">
-                    <a class="nav-link" href="/pages/telefones">Telefones Úteis</a>
-                </li>
+                {{Route::currentRouteName()}}
 
-{{--
-
-                <li class="nav-item">
-                    <a class="nav-link" href="http://transparencia.alerj.rj.gov.br/protocolo" target="_blank">Protocolo</a>
+                <li class="nav-item {{Route::currentRouteName() == 'home' ? 'active' : ''}}" >
+                    <a class="nav-link" aria-current="page" href="{{route('home')}}">Home</a>
                 </li>
---}}
-                <li class="nav-item">
+                <li class="nav-item {{Route::currentRouteName() == 'pages.aloalerj' ? 'active' : ''}}">
+                    <a class="nav-link" href="{{route('pages.aloalerj')}}">O Alô Alerj</a>
+                </li>
+                <li class="nav-item {{Route::currentRouteName() == 'pages.committees' ? 'active' : ''}}">
+                    <a class="nav-link" href="{{route('pages.committees')}}">Comissões</a>
+                </li>
+                <li class="nav-item {{Route::currentRouteName() == 'pages.telefones' ? 'active' : ''}}">
+                    <a class="nav-link" href="{{route('pages.telefones')}}">Telefones Úteis</a>
+                </li>
+                <li class="nav-item {{Route::currentRouteName() == 'contact.index' ? 'active' : ''}}">
                     <a class="nav-link" href="/contact" title="Contato"> <i class="fa fa-envelope-o envelopemenu" aria-hidden="true"></i> Contato</a>
                 </li>
-
-{{--
-                <li class="nav-item">
-                    <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Disabled</a>
-                </li>
-
-                <li class="nav-item dropdown">
-                    <a class="nav-link dropdown-toggle" href="#" id="dropdown04" data-bs-toggle="dropdown" aria-expanded="false">Dropdown</a>
-                    <ul class="dropdown-menu" aria-labelledby="dropdown04">
-                        <li><a class="dropdown-item" href="#">Action</a></li>
-                        <li><a class="dropdown-item" href="#">Another action</a></li>
-                        <li><a class="dropdown-item" href="#">Something else here</a></li>
-                    </ul>
-                </li>
---}}
-
             </ul>
-
-{{--
-            <form>
-                <input class="form-control" type="text" placeholder="Search" aria-label="Search">
-            </form>
---}}
-
         </div>
     </div>
 </nav>
