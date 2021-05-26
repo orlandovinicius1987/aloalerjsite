91a8fbe293be12d5f71b248540a86d82c4f65b32
Breno Trengrouse <brenot@gmail.com>
2021-05-26 13:25:05 -0300

    Issues 5

--- resources/views/partials/telegram-icons.blade.php
+++ resources/views/partials/telegram-icons.blade.php
@@ -1,18 +1,22 @@
 
-    <div class="col-6 offset-3 >
-        <a href="https://wa.me/5521988904742" target="_blank" class="whatsapp">
-            <img src="/templates/mv/svg/whatsapp.svg" class="img-responsive" alt="whatsapp">
-            <p class="leg-whatsapp">WhatsApp</p>
-        </a>
-    </div>
+<div class="col-6 offset-3" >
 
-    {{--
 
-    <div class="col-4 col-md-3">
-        <a href="https://t.me/aloalerjatendimento" target="_blank" class="telegram">
-            <img src="/templates/mv/svg/telegram.svg" class="img-responsive" alt="telegram">
-            <p class="leg-telegram">Telegram</p>
-        </a>
-    </div>
+    <a href="//api.whatsapp.com/send?phone=55%2021%2098890%204742" target="_blank" class="whatsapp">
+        <img src="/templates/mv/svg/whatsapp.svg" class="img-responsive" alt="whatsapp">
+        <p class="leg-whatsapp">WhatsApp</p>
+    </a>
+
+
+</div>
+
+{{--
+
+<div class="col-4 col-md-3">
+    <a href="https://t.me/aloalerjatendimento" target="_blank" class="telegram">
+        <img src="/templates/mv/svg/telegram.svg" class="img-responsive" alt="telegram">
+        <p class="leg-telegram">Telegram</p>
+    </a>
+</div>
 
 --}}
